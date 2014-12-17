beb199bb14faed952cd0a0e002cf5a735ce1c327
Vliedel <bart@dobots.nl>
2014-12-17 17:51:50 +0100

    added analog reference pin

--- include/common/boards.h
+++ include/common/boards.h
@@ -33,11 +33,10 @@
 
 #if(BOARD==NRF6310)
 
-#define PIN_GPIO_LED         8                   // this is p1.0
-#define PIN_AIN_ADC          2                   // ain2 is p0.1
+#define PIN_GPIO_LED         8                   // this is p1.0 or gpio 8
+#define PIN_AIN_ADC          2                   // ain2 is p0.1 or gpio 1
 #define PIN_GPIO_RX          16
 #define PIN_GPIO_TX          17
-
 #define PIN_AIN_LPCOMP       3                   // ain3 is p0.2 or gpio 2
 
 #endif
@@ -60,6 +59,7 @@
 #define PIN_GPIO_LED         18                  // this is p0.18 or gpio 18
 #define PIN_AIN_ADC          2                   // ain2 is p0.01 or gpio 1
 #define PIN_AIN_LPCOMP       3                   // ain3 is p0.02 or gpio 2
+#define PIN_AIN_LPCOMP_REF   0                   // ref0 is p0.00 or gpio 0
 #define PIN_GPIO_RX          11                  // this is p0.11 or gpio 11
 #define PIN_GPIO_TX          9                   // this is p0.09 or gpio 9
 #define PIN_GPIO_LED_CON     19					 // shows connection state on the evaluation board
