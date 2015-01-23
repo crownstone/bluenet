867b326592eb4212be716ba755bc9f60780d6434
Vliedel <bart@dobots.nl>
2015-01-23 17:59:03 +0100

    removed some old code

--- include/BluetoothLE.h
+++ include/BluetoothLE.h
@@ -44,15 +44,6 @@ extern "C" {
 #include <util/function.h>
 #include <util/utils.h>
 
-
-//using std::vector;
-//using std::string;
-//using std::exception;
-
-const uint8_t sin_table[] = {0, 0,1,2,4,6,9,12,16,20,24,29,35,40,	46,	53,	59,	66,	74,	81,	88,	96,	104,112,120,128,136,144,152,160,168,175,182,190,197,203,210,216,221,227,
-	232,236,240,244,247,250,252,254,255,255,255,255,255,254,252,250,247,244,240,236,232,227,221,216,210,203,197,190,182,175,168,160,152,144,136,128,120,112,104,
-	96,88,81,74,66,59,	53,	46,	40,	35,	29,24,	20,	16,	12,	9,	6,	4,	2,1,0};
-
 // TOOD replace std::vector with a fixed, in place array of size capacity.
 template<typename T> class tuple : public std::vector<T> {
   public:
