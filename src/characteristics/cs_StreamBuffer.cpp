3a409164139553d26fa72f68fb83a694d7be9d02
Dominik Egger <dominik@almende.org>
2015-03-02 15:49:51 +0100

    BugFix for StreamBuffer

--- src/characteristics/cs_StreamBuffer.cpp
+++ src/characteristics/cs_StreamBuffer.cpp
@@ -7,20 +7,19 @@
 
 #include "characteristics/cs_StreamBuffer.h"
 
-
 StreamBuffer::StreamBuffer() : _type(0), _plength(0) {
-	_payload = (uint8_t*)calloc(MAX_BUFFER_SERIALIZED_SIZE, sizeof(uint8_t));
+	_payload = (uint8_t*)calloc(MAX_BUFFER_SIZE, sizeof(uint8_t));
 }
 
 bool StreamBuffer::operator!=(const StreamBuffer &other) {
 	if (_type != other._type) return true;
 	if (_plength != other._plength) return true;
-	if (memcmp(_payload, other._payload, MAX_BUFFER_SERIALIZED_SIZE) != 0) return true;
+	if (memcmp(_payload, other._payload, MAX_BUFFER_SIZE) != 0) return true;
 	return false;
 }
 
 uint16_t StreamBuffer::getSerializedLength() const {
-	return MAX_BUFFER_SERIALIZED_SIZE;
+	return MAX_BUFFER_SIZE + BUFFER_HEADER_SIZE;
 }
 
 bool StreamBuffer::toString(std::string &str) {
@@ -32,21 +31,28 @@ bool StreamBuffer::toString(std::string &str) {
 bool StreamBuffer::fromString(const std::string &str) {
 	_plength = str.length();
 	_plength = 
-		((_plength > MAX_BUFFER_SERIALIZED_SIZE) ? MAX_BUFFER_SERIALIZED_SIZE : _plength); 
+		((_plength > MAX_BUFFER_SIZE) ? MAX_BUFFER_SIZE : _plength);
 	memcpy(_payload, str.c_str(), _plength*sizeof(uint8_t));
 }
 	
 void StreamBuffer::setPayload(uint8_t *payload, uint8_t plength) {
 	_plength = 
-		((plength > MAX_BUFFER_SERIALIZED_SIZE) ? MAX_BUFFER_SERIALIZED_SIZE : plength); 
+		((plength > MAX_BUFFER_SIZE) ? MAX_BUFFER_SIZE : plength);
 	memcpy(_payload, payload, _plength*sizeof(uint8_t));
 }
 
 uint8_t StreamBuffer::add(uint8_t value) {
-	if (_plength >= MAX_BUFFER_SERIALIZED_SIZE - 1) return 1;
-	_plength++;
-	_payload[_plength] = value;
+	if (_plength >= MAX_BUFFER_SIZE) return 1;
+	_payload[_plength++] = value;
 	return 0;
+}
+
+void StreamBuffer::clear() {
+	if (_payload) {
+		free(_payload);
+	}
+	_payload = (uint8_t*)calloc(MAX_BUFFER_SIZE, sizeof(uint8_t));
+	_plength = 0;
 }
 
 void StreamBuffer::serialize(uint8_t* buffer, uint16_t length) const {
@@ -55,6 +61,7 @@ void StreamBuffer::serialize(uint8_t* buffer, uint16_t length) const {
 	uint8_t *ptr = buffer;
 	*ptr++ = _type;
 	*ptr++ = _plength;
+
 	if (_plength) memcpy(ptr, _payload, _plength*sizeof(uint8_t));
 }
 
@@ -64,9 +71,9 @@ void StreamBuffer::deserialize(uint8_t* buffer, uint16_t length) {
 	uint8_t *ptr = buffer;
 	_type = *ptr++;
 	_plength = *ptr++;
-	memset(_payload, 0, MAX_BUFFER_SERIALIZED_SIZE);
+	memset(_payload, 0, MAX_BUFFER_SIZE);
 	uint16_t corr_plength = 
-		((_plength > MAX_BUFFER_SERIALIZED_SIZE) ? MAX_BUFFER_SERIALIZED_SIZE : _plength); 
+		((_plength > MAX_BUFFER_SIZE) ? MAX_BUFFER_SIZE : _plength);
 	memcpy(_payload, ptr, corr_plength*sizeof(uint8_t));
 }
 
