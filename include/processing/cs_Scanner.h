99c227572e4aad82af8459b55c521143edd81295
Dominik Egger <dominik@almende.org>
2015-12-03 10:55:06 +0100

    added Scanner class

---
+++ include/processing/cs_Scanner.h
@@ -0,0 +1,61 @@
+/**
+ * Author: Dominik Egger
+ * Copyright: Distributed Organisms B.V. (DoBots)
+ * Date: Dec 2, 2015
+ * License: LGPLv3+
+ */
+#pragma once
+
+#include <ble/cs_Stack.h>
+#include <structs/cs_ScanResult.h>
+
+#define SCAN_DURATION 2000 // 1 sec
+#define SCAN_BREAK 10000 // 4 sec
+
+using namespace BLEpp;
+
+class Scanner {
+
+public:
+
+	Scanner(Nrf51822BluetoothStack* stack);
+	virtual ~Scanner();
+
+	void onBleEvent(ble_evt_t * p_ble_evt);
+
+	void manualStartScan();
+	void manualStopScan();
+
+	bool isScanning();
+
+	ScanResult* getResults();
+
+	static void staticTick(Scanner* ptr);
+
+	void start();
+	void stop();
+
+private:
+
+	enum SCAN_OP_CODE {
+		SCAN_START, SCAN_STOP
+	};
+
+	SCAN_OP_CODE _opCode;
+
+	bool _scanning;
+	bool _running;
+
+	app_timer_id_t _appTimerId;
+
+	Nrf51822BluetoothStack* _stack;
+
+	uint8_t _scanBuffer[sizeof(peripheral_device_list_t)];
+	ScanResult* _scanResult;
+
+	void executeScan();
+	void sendResults();
+
+	void onAdvertisement(ble_gap_evt_adv_report_t* p_adv_report);
+};
+
---
+++ src/processing/cs_Scanner.cpp
@@ -0,0 +1,134 @@
+/**
+ * Author: Dominik Egger
+ * Copyright: Distributed Organisms B.V. (DoBots)
+ * Date: Dec 2, 2015
+ * License: LGPLv3+
+ */
+#include "processing/cs_Scanner.h"
+
+#include <protocol/cs_MeshControl.h>
+
+Scanner::Scanner(Nrf51822BluetoothStack* stack) : _opCode(SCAN_START), _scanning(false), _running(false), _stack(stack) {
+
+	_scanResult = new ScanResult();
+
+//	MasterBuffer& mb = MasterBuffer::getInstance();
+//	buffer_ptr_t buffer = NULL;
+//	uint16_t maxLength = 0;
+//	mb.getBuffer(buffer, maxLength);
+
+	_scanResult->assign(_scanBuffer, sizeof(_scanBuffer));
+
+	Timer::getInstance().createSingleShot(_appTimerId, (app_timer_timeout_handler_t)Scanner::staticTick);
+}
+
+Scanner::~Scanner() {
+	// TODO Auto-generated destructor stub
+}
+
+void Scanner::manualStartScan() {
+
+	LOGi("Init scan result");
+	_scanResult->clear();
+	_scanning = true;
+
+	if (!_stack->isScanning()) {
+		LOGi("start scanning...");
+		_stack->startScanning();
+	}
+}
+
+void Scanner::manualStopScan() {
+
+	_scanning = false;
+
+	if (_stack->isScanning()) {
+		LOGi("stop scanning...");
+		_stack->stopScanning();
+	}
+}
+
+bool Scanner::isScanning() {
+	return _scanning && _stack->isScanning();
+}
+
+ScanResult* Scanner::getResults() {
+	return _scanResult;
+}
+
+void Scanner::staticTick(Scanner* ptr) {
+	ptr->executeScan();
+}
+
+void Scanner::start() {
+	_running = true;
+	_opCode = SCAN_START;
+	executeScan();
+}
+
+void Scanner::stop() {
+	_opCode = SCAN_STOP;
+	executeScan();
+	_running = false;
+}
+
+void Scanner::executeScan() {
+
+	if (!_running) return;
+
+	LOGi("executeScan");
+	switch(_opCode) {
+	case SCAN_START: {
+		LOGi("START");
+
+		// start scanning
+		manualStartScan();
+
+		// set timer to trigger in SCAN_DURATION sec, then stop again
+		Timer::getInstance().start(_appTimerId, MS_TO_TICKS(SCAN_DURATION), this);
+
+		_opCode = SCAN_STOP;
+		break;
+	}
+	case SCAN_STOP: {
+		LOGi("STOP");
+
+		// stop scanning
+		manualStopScan();
+
+		_scanResult->print();
+
+		// sendResults
+		sendResults();
+
+		// set timer to trigger in SCAN_BREAK sec, then start again
+		Timer::getInstance().start(_appTimerId, MS_TO_TICKS(SCAN_BREAK), this);
+
+		_opCode = SCAN_START;
+		break;
+	}
+	}
+
+}
+
+void Scanner::sendResults() {
+	MeshControl::getInstance().sendScanMessage(_scanResult->getList()->list, _scanResult->getSize());
+}
+
+void Scanner::onBleEvent(ble_evt_t * p_ble_evt) {
+
+	switch (p_ble_evt->header.evt_id) {
+		case BLE_GAP_EVT_ADV_REPORT:
+		onAdvertisement(&p_ble_evt->evt.gap_evt.params.adv_report);
+		break;
+	}
+}
+
+void Scanner::onAdvertisement(ble_gap_evt_adv_report_t* p_adv_report) {
+
+	if (isScanning()) {
+		_scanResult->update(p_adv_report->peer_addr.addr, p_adv_report->rssi);
+	} else {
+		LOGw("why we getting advertisements if scan is not running?");
+	}
+}
