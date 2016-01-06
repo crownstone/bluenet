13b893a745a5fa5aee1b613cccf9e67594a9c20d
Dominik Egger <dominik@almende.org>
2016-01-06 17:04:26 +0100

    added DEVICE_INFO_SERVICE to CMakeBuild.config to enable/disable Device Information Service

--- src/cs_main_crownstone.cpp
+++ src/cs_main_crownstone.cpp
@@ -149,8 +149,11 @@ void Crownstone::configDrivers() {
 void Crownstone::createServices() {
 	LOGi("Create all services");
 
+// should be available always, but for now, only enable if required
+#if DEVICE_INFO_SERVICE==1
 	_deviceInformationService = new DeviceInformationService();
 	_stack->addService(_deviceInformationService);
+#endif
 
 #if GENERAL_SERVICE==1 || DEVICE_TYPE==DEVICE_FRIDGE
 	// general services, such as internal temperature, setting names, etc.
