c07fb3fdc32918e4c54c80f7394161adf346ef23
Anne van Rossum <anne@dobots.nl>
2016-03-01 18:50:10 +0100

    Update document generation

--- scripts/publish_doc.sh
+++ scripts/publish_doc.sh
@@ -8,6 +8,6 @@ git add -u .
 
 git commit
 
-git subtree push --prefix docs/html upstream gh-pages
+git subtree push --prefix docs/html origin gh-pages
 
 echo "Go to https://dobots.github.com/bluenet"
