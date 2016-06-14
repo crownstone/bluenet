8c691b99f06b11891eca2e3224b09db9f8f6c6c7
Vliedel <bart@dobots.nl>
2016-06-14 15:56:14 +0200

    Use relay when it's there.

--- include/processing/cs_TemperatureGuard.h
+++ include/processing/cs_TemperatureGuard.h
@@ -41,9 +41,11 @@ public:
 	void tick() {
 		if (getTemperature() > _maxTemp) {
 			//! Switch off all channels
-			PWM::getInstance().switchOff();
-			//! Make sure pwm can't be set anymore
-			PWM::getInstance().deinit();
+			Switch::getInstance().pwmOff();
+			Switch::getInstance().turnOff();
+//			PWM::getInstance().switchOff();
+//			//! Make sure pwm can't be set anymore
+//			PWM::getInstance().deinit();
 		}
 		// TODO: make next time to next tick depend on current temperature
 		scheduleNextTick();
