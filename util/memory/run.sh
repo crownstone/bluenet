e8b732a6554c1aedf8816771fbc86a735c66cd29
Anne van Rossum <anne@crownstone.rocks>
2016-08-15 11:27:43 +0200

    Use 512kB rather than only 256kB

--- util/memory/run.sh
+++ util/memory/run.sh
@@ -3,4 +3,4 @@
 echo "Make sure you did run ../../scripts/map_update.sh once"
 
 options="--new-window --allow-file-access-from-files"
-google-chrome $options index.html
+gnome-open $options index.html
