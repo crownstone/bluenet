eaf8d23aaa745db9053ad11db3b6d30c9f4bebb9
Anne van Rossum <anne@crownstone.rocks>
2016-09-02 14:42:09 +0200

    Add config script for Bluetooth with recent bluez

--- scripts/upload_over_the_air.sh
+++ scripts/upload_over_the_air.sh
@@ -6,8 +6,11 @@ source $path/_config.sh
 #PROGRAM=crownstone.hex
 #ADDRESS=F2:E1:DE:0D:92:30
 
-PROGRAM=combined.hex
-ADDRESS=F4:60:EF:35:22:EC
+#PROGRAM=combined.hex
+#ADDRESS=F4:60:EF:35:22:EC
+
+PROGRAM=crownstone.hex
+ADDRESS=F8:86:A5:56:42:51
 
 #TODO: make this configurable? - No, move this script to the dfu_linux repo
 cd /opt/nrf51_dfu_linux
