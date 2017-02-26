f2ecf3c62e708646ca1f3ad2a3cc9899c9904c48
Anne van Rossum <anne@crownstone.rocks>
2017-02-27 00:34:01 +0100

    Create also target folder in config/build/bin directories.

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -4,15 +4,16 @@ script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $script_path/env.sh
 
-mkdir -p $BLUENET_CONFIG_DIR
-mkdir -p $BLUENET_BUILD_DIR
-mkdir -p $BLUENET_BIN_DIR
-mkdir -p $BLUENET_RELEASE_DIR
-mkdir -p $BLUENET_BOOTLOADER_DIR
-
 target="test"
 
-cfile="${BLUENET_CONFIG_DIR}/${target}"
+mkdir -p "${BLUENET_CONFIG_DIR}/${target}"
+mkdir -p "${BLUENET_BUILD_DIR}/${target}"
+mkdir -p "${BLUENET_BIN_DIR}/${target}"
+
+mkdir -p "${BLUENET_RELEASE_DIR}"
+mkdir -p "${BLUENET_BOOTLOADER_DIR}"
+
+cfile="${BLUENET_CONFIG_DIR}/${target}/CMakeBuild.config"
 
 echo 'BLUETOOTH_NAME="test"' >> $cfile
 echo 'COMPILER_PATH=/usr' >> $cfile
