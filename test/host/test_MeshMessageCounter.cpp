b907f1df5fc72364f220151a6fbf284d13d71b06
Vliedel <bart@dobots.nl>
2017-05-11 11:10:51 +0200

    Use climits instead?

--- test/host/test_MeshMessageCounter.cpp
+++ test/host/test_MeshMessageCounter.cpp
@@ -10,16 +10,17 @@
 #include <iostream>
 //#define __STDC_LIMIT_MACROS
 //#include <stdint.h>
+#include <climits>
 #include <cstdint>
-#include <limits>
+//#include <limits>
 
 using namespace std;
 
 int main() {
 	cout << "Test MeshMessageCounter implementation" << endl;
 
-	uint32_t uint32_Max = std::numeric_limits<uint32_t>::max();
-	uint32_t uint16_Max = std::numeric_limits<uint16_t>::max();
+//	uint32_t uint32_Max = std::numeric_limits<uint32_t>::max();
+//	uint32_t uint16_Max = std::numeric_limits<uint16_t>::max();
 
 	MeshMessageCounter msgCounter;
 	MeshMessageCounter msgCounter2;
@@ -27,8 +28,8 @@ int main() {
 	uint32_t val = msgCounter.getVal();
 	cout << "counter start: " << val << endl;
 
-//	msgCounter.setVal(UINT32_MAX-1);
-	msgCounter.setVal(uint32_Max-1);
+	msgCounter.setVal(UINT32_MAX-1);
+//	msgCounter.setVal(uint32_Max-1);
 	cout << "set: " << msgCounter.getVal() << endl;
 
 
@@ -43,28 +44,28 @@ int main() {
 
 	// New is larger than old
 	msgCounter.setVal(1000);
-//	msgCounter2.setVal(UINT32_MAX - 1000);
-	msgCounter2.setVal(uint32_Max - 1000);
+	msgCounter2.setVal(UINT32_MAX - 1000);
+//	msgCounter2.setVal(uint32_Max - 1000);
 	delta = msgCounter.calcDelta(msgCounter2.getVal());
 	cout << "delta: " << msgCounter2.getVal() << " - " << msgCounter.getVal() << " = " << delta << endl;
 
 	// New is smaller than old, but newer
 	msgCounter.setVal(1000);
-//	msgCounter2.setVal(UINT16_MAX);
-	msgCounter2.setVal(uint16_Max);
+	msgCounter2.setVal(UINT16_MAX);
+//	msgCounter2.setVal(uint16_Max);
 	delta = msgCounter.calcDelta(msgCounter2.getVal());
 	cout << "delta: " << msgCounter2.getVal() << " - " << msgCounter.getVal() << " = " << delta << endl;
 
 	// New is smaller than old
-//	msgCounter.setVal(UINT32_MAX - 1000);
-	msgCounter.setVal(uint32_Max - 1000);
+	msgCounter.setVal(UINT32_MAX - 1000);
+//	msgCounter.setVal(uint32_Max - 1000);
 	msgCounter2.setVal(1000);
 	delta = msgCounter.calcDelta(msgCounter2.getVal());
 	cout << "delta: " << msgCounter2.getVal() << " - " << msgCounter.getVal() << " = " << delta << endl;
 
 	// New is larger than old, but older
-//	msgCounter.setVal(UINT16_MAX);
-	msgCounter.setVal(uint16_Max);
+	msgCounter.setVal(UINT16_MAX);
+//	msgCounter.setVal(uint16_Max);
 	msgCounter2.setVal(1000);
 	delta = msgCounter.calcDelta(msgCounter2.getVal());
 	cout << "delta: " << msgCounter2.getVal() << " - " << msgCounter.getVal() << " = " << delta << endl;
@@ -77,8 +78,8 @@ int main() {
 
 	// Old is not in loop yet, int32 overflow
 	msgCounter.setVal(1);
-//	msgCounter2.setVal(UINT32_MAX);
-	msgCounter2.setVal(uint32_Max);
+	msgCounter2.setVal(UINT32_MAX);
+//	msgCounter2.setVal(uint32_Max);
 	delta = msgCounter.calcDelta(msgCounter2.getVal());
 	cout << "delta: " << msgCounter2.getVal() << " - " << msgCounter.getVal() << " = " << delta << endl;
 
@@ -89,8 +90,8 @@ int main() {
 	cout << "delta: " << msgCounter2.getVal() << " - " << msgCounter.getVal() << " = " << delta << endl;
 
 	// New is not in loop yet, int32 overflow
-//	msgCounter.setVal(UINT32_MAX);
-	msgCounter.setVal(uint32_Max);
+	msgCounter.setVal(UINT32_MAX);
+//	msgCounter.setVal(uint32_Max);
 	msgCounter2.setVal(1);
 	delta = msgCounter.calcDelta(msgCounter2.getVal());
 	cout << "delta: " << msgCounter2.getVal() << " - " << msgCounter.getVal() << " = " << delta << endl;
