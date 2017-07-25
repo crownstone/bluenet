a2e6aedc576433334ead4b49e2e7d4b93fba92e8
Anne van Rossum <anne@crownstone.rocks>
2017-07-25 14:54:10 +0200

    Check actually for firmware version on the hardware itself.

--- scripts/_readbyte.sh
+++ scripts/_readbyte.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $path/_utils.sh
-source $path/_config.sh
+source $path/_utils.sh >/dev/null
+source $path/_config.sh >/dev/null
 
 SCRIPT_DIR=$path/jlink
 TEMP_DIR=$path/tmp
@@ -14,8 +14,8 @@ DEVICE=nRF52832_xxAA
 
 sed "s|@ADDRESS@|$ADDRESS|" $SCRIPT_DIR/readbyte.script > $TEMP_DIR/readbyte.script
 
-echo "$JLINK -Device $DEVICE -speed 4000 -If SWD $TEMP_DIR/readbyte.script"
-$JLINK -Device $DEVICE -speed 4000 -If SWD $TEMP_DIR/readbyte.script
+#echo "$JLINK -Device $DEVICE -speed 4000 -If SWD $TEMP_DIR/readbyte.script"
+$JLINK -Device $DEVICE -speed 4000 -If SWD $TEMP_DIR/readbyte.script | grep -A1 identified | tail -n1 | cut -f2 -d'=' | tr -d ' '
 
-echo "The result should be something like:"
-echo "    1000005C = 3C 00 FF FF"
+#echo "The result should be something like:"
+#echo "    1000005C = 3C 00 FF FF"
--- scripts/firmware.sh
+++ scripts/firmware.sh
@@ -174,12 +174,11 @@ build() {
 writeHardwareVersion() {
 	verifyHardwareBoardDefined
 	if [ $? -eq 0 ]; then
-		# cs_info "HARDWARE_BOARD=$HARDWARE_BOARD"
 		HARDWARE_BOARD_INT=`cat $BLUENET_DIR/include/cfg/cs_Boards.h | grep -o "#define.*\b$HARDWARE_BOARD\b.*" | grep -w "$HARDWARE_BOARD" | awk 'NF>1{print $NF}'`
 		if [ $? -eq 0 ] && [ -n "$HARDWARE_BOARD_INT" ]; then
-				cs_info "HARDWARE_BOARD $HARDWARE_BOARD = $HARDWARE_BOARD_INT"
-				${path}/_writebyte.sh $HARDWARE_BOARD_ADDRESS `printf "%x" $HARDWARE_BOARD_INT` $serial_num
-				checkError "Error writing hardware version"
+			cs_info "HARDWARE_BOARD $HARDWARE_BOARD = $HARDWARE_BOARD_INT"
+			${path}/_writebyte.sh $HARDWARE_BOARD_ADDRESS `printf "%x" $HARDWARE_BOARD_INT` $serial_num
+			checkError "Error writing hardware version"
 		else
 			cs_err "Failed to extract HARDWARE_BOARD=$HARDWARE_BOARD from $BLUENET_DIR/include/cfg/cs_Boards.h"
 		fi
@@ -187,7 +186,7 @@ writeHardwareVersion() {
 }
 
 upload() {
-	# verifyHardwareBoardDefined
+	verifyHardwareBoardDefined
 
 	if [ $? -eq 0 ]; then
 		# writeHardwareVersion
@@ -297,6 +296,12 @@ verifyHardwareBoardDefined() {
 		cs_err "for a given target, or by calling the script as"
 		cs_err "   HARDWARE_BOARD=... ./firmware.sh"
 		exit 1
+	fi 
+	echo "Find hardware board version via ${path}/_readbyte.sh $HARDWARE_BOARD_ADDRESS $serial_num"
+	version=$(${path}/_readbyte.sh $HARDWARE_BOARD_ADDRESS $serial_num)
+	if [ $version == 'FFFFFFFF' ]; then
+	  cs_err "You have to write the hardware version! It is still set to $version."
+	  exit 1
 	fi
 }
 
@@ -333,6 +338,9 @@ case "$cmd" in
 		;;
 	release)
 		release
+		;;
+	readHardwareVersion)
+		verifyHardwareBoardDefined
 		;;
 	writeHardwareVersion)
 		writeHardwareVersion
