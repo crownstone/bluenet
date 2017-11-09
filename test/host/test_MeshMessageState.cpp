1fe20053860c7c17c3f723bfb8c7666d2c727e0c
Vliedel <bart@dobots.nl>
2017-11-09 17:31:54 +0100

    Fixed unit test.

--- test/host/test_MeshMessageState.cpp
+++ test/host/test_MeshMessageState.cpp
@@ -15,12 +15,12 @@ int main() {
 
 	sm.clear();
 
-	state_item_t item0 = { 0, 1, 100, 50};
-	state_item_t item1 = { 1, 1, 120, 40};
-	state_item_t item2 = { 2, 0, 220, 40};
-	state_item_t item3 = { 3, 0, 130, 20};
-	state_item_t item4 = { 4, 1, 30, 30};
-	state_item_t item5 = { 5, 1, 100, 100};
+	state_item_t item0 = { 0, 1, 1024, 100, 50};
+	state_item_t item1 = { 1, 1, 1024, 120, 40};
+	state_item_t item2 = { 2, 0, 1024, 220, 40};
+	state_item_t item3 = { 3, 0, 1024, 130, 20};
+	state_item_t item4 = { 4, 1, 1024, 30, 30};
+	state_item_t item5 = { 5, 1, 1024, 100, 100};
 
 	sm.push_back(item0);
 	sm.push_back(item1);
@@ -29,10 +29,10 @@ int main() {
 	sm.push_back(item4);
 	sm.push_back(item5);
 
-	cout << "Power usage [0]: " << smt.list[0].powerUsage << endl;
-	cout << "Power usage [1]: " << smt.list[1].powerUsage << endl;
-	cout << "Power usage [2]: " << smt.list[2].powerUsage << endl;
-	cout << "Power usage [3]: " << smt.list[3].powerUsage << endl;
+	cout << "Power usage [0]: " << smt.list[0].powerUsageApparant << endl;
+	cout << "Power usage [1]: " << smt.list[1].powerUsageApparant << endl;
+	cout << "Power usage [2]: " << smt.list[2].powerUsageApparant << endl;
+	cout << "Power usage [3]: " << smt.list[3].powerUsageApparant << endl;
 
 	cout << "Head: " << (int)smt.head << endl;
 	cout << "Tail: " << (int)smt.tail << endl;
@@ -40,6 +40,6 @@ int main() {
 
 	for (state_message::iterator iter = sm.begin(); iter != sm.end(); ++iter) {
 		state_item_t si = *iter;
-		cout << "Power usage [" <<  si.id << "]: " << si.powerUsage << endl;
+		cout << "Power usage [" <<  si.id << "]: " << si.powerUsageApparant << endl;
 	}
 }
