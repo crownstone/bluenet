05ce4c16399f04726331a7e0cdbcba69f595f004
Vliedel <bart@dobots.nl>
2018-12-13 16:23:50 +0100

    Added proto boards.

--- include/cfg/cs_Boards.h
+++ include/cfg/cs_Boards.h
@@ -50,9 +50,7 @@ extern "C" {
 #define ACR01B1C             1002
 #define ACR01B1D             1003 // released
 #define ACR01B1E             1004 // same as ACR01B1D, but with patch
-//#define ACR01B6C             1005 // prototype, deprecated
-//#define ACR01B6D             1006 // prototype, deprecated
-#define ACR01B9C             1007 // prototype
+#define ACR01B9E             1007 // prototype
 
 // CROWNSTONE PLUGS
 
