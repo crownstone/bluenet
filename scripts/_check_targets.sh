abf3dbba42e365834186ef54adcecfb37764744a
vliedel <bart@crownstone.rocks>
2019-02-19 15:35:37 +0100

    Todo: check_targets gives a warning when target was already set (as config_dir was changed).

--- scripts/_check_targets.sh
+++ scripts/_check_targets.sh
@@ -2,27 +2,23 @@
 #
 # check if _targets.sh is present in the BLUENET_CONFIG_DIR. See _targets_template.sh for explanations
 
-# if [[ -z $2 ]]; then
-# 	# if no target provided, set default target to crownstone
-# 	target="crownstone"
-# else
-# 	target=$2
-# fi
-
-#target if provided, otherwise default "crownstone"
+# Target if provided, otherwise default "crownstone"
 target=${1:-crownstone}
 
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $path/_utils.sh
 
 if [[ -e "$BLUENET_CONFIG_DIR/_targets.sh" ]]; then
-	#adjusts target and sets serial_num
+	# Adjusts target and sets serial_num
 	source $BLUENET_CONFIG_DIR/_targets.sh $target
+	target_set=true
 else
 	if [[ $target != "crownstone" ]]; then
 		cs_err "To use different targets, copy _targets_template.sh to $BLUENET_CONFIG_DIR and rename to _targets.sh"
 		exit 1
 	else
+		cs_log "Not found: $BLUENET_CONFIG_DIR/_targets.sh"
+		cs_log "You can ignore this when config dir was already changed to correct target."
 		cs_warn "NOTE: this is the old way of calling the scripts, you might want to check _targets_template.sh or the README"
 	fi
 fi
