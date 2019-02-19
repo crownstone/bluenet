39dd2912c4a3164b97a535e9bf9f5f4ba20d27cc
vliedel <bart@crownstone.rocks>
2019-02-19 15:25:54 +0100

    Add options to combine.sh.

--- scripts/combine.sh
+++ scripts/combine.sh
@@ -12,13 +12,77 @@
 #   Date: Feb. 3, 2014
 #######################################################################################################################
 
-target=${1:-crownstone}
+usage() {
+	echo "Combines multiple binaries into a single binary"
+	echo
+	echo "What to include:"
+	echo "   -f, --firmware                       include firmware"
+	echo "   -b, --bootloader                     include bootloader"
+	echo "   -s, --softdevice                     include softdevice"
+	echo "   -h, --hardware_verion                include hardware version"
+	echo
+	echo "Extra arguments:"
+	echo "   -t target, --target target           specify config target (files are generated in separate directories)"
+}
 
-# TODO: Check if $target configuration folder actually exists...
+getopt --test > /dev/null
+if [[ $? -ne 4 ]]; then
+	echo "I’m sorry, `getopt --test` failed in this environment."
+	exit $ERR_GETOPT_TEST
+fi
 
+SHORT=t:fbsh
+LONG=target:firmware,bootloader,softdevice,hardware_version
+
+PARSED=$(getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@")
+if [[ $? -ne 0 ]]; then
+	exit $ERR_GETOPT_PARSE
+fi
+
+eval set -- "$PARSED"
+
+while true; do
+	case "$1" in
+		-t|--target) 
+			target=$2
+			shift 2
+			;;
+		-f|--firmware)
+			include_firmware=true
+			shift 1
+			;;
+		-b|--bootloader)
+			include_bootloader=true
+			shift 1
+			;;
+		-s|--softdevice)
+			include_softdevice=true
+			shift 1
+			;;
+		-h|--hardware_version)
+			include_hardware_version=true
+			shift 1
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			echo "Error in arguments."
+			echo $2
+			exit $ERR_ARGUMENTS
+			;;
+	esac
+done
+
+target=${target:-crownstone}
+
+# Use the current path as the bluenet directory
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $path/_utils.sh
+
 export BLUENET_DIR=$(readlink -m ${path}/..)
+
 cs_info "Load configuration from: ${path}/_check_targets.sh $target"
 source ${path}/_check_targets.sh $target
 
@@ -27,19 +91,15 @@ source $path/_config.sh
 
 cd ${BLUENET_BIN_DIR} && rm -f combined*
 
-add_bootloader=true
-add_softdevice=true
-add_binary=true
-add_board_info=true
 
-if [[ "$add_bootloader" == true ]]; then
+if [[ $include_bootloader ]]; then
 	# These settings are already incorporated in the bootloader.hex binary, so no need to add them here as well
 	#BOOTLOADER_SETTINGS="-exclude 0x3FC00 0x3FC20 -generate 0x3FC00 0x3FC04 -l-e-constant 0x01 4 -generate 0x3FC04 0x3FC08 -l-e-constant 0x00 4 -generate 0x3FC08 0x3FC0C -l-e-constant 0xFE 4 -generate 0x3FC0C 0x3FC20 -constant 0x00"
 
 	ADD_BOOTLOADER="bootloader.hex -intel"
 fi
 
-if [[ "$add_board_info" == true ]]; then
+if [[ $include_hardware_version ]]; then
 	BOARD_FILE="$BLUENET_DIR/include/cfg/cs_Boards.h"
 	cs_info "Obtain hardware board from $BOARD_FILE"
 	HARDWARE_BOARD_INT=$(grep -oP "#define\s+$HARDWARE_BOARD\s+\d+" $BOARD_FILE | grep -oP "\d+$")
@@ -52,10 +112,10 @@ if [[ "$add_board_info" == true ]]; then
 	HARDWARE_BOARD_CONFIG="-exclude 0x10001084 0x10001088 -generate 0x10001084 0x10001088 -l-e-constant $HARDWARE_BOARD_HEX 4"
 fi
 
-if [[ "$add_softdevice" == true ]]; then
+if [[ $include_softdevice ]]; then
 	ADD_SOFTDEVICE="${SOFTDEVICE_DIR}/${SOFTDEVICE_DIR_HEX}/${SOFTDEVICE}_softdevice.hex -intel"
 fi
-if [[ "$add_binary" == true ]]; then
+if [[ $include_firmware ]]; then
 	ADD_BINARY="crownstone.bin -binary -offset $APPLICATION_START_ADDRESS"
 	BINARY_VALID_FLAG="-exclude 0x7F000 0x7F004 -generate 0x7F000 0x7F004 -le-constant 0x00000001 4"
 fi
@@ -69,5 +129,3 @@ if [ ! $res -eq 0 ]; then
 fi
 
 cs_log "Combined binary is: ${BLUENET_BIN_DIR}/combined.hex"
-cs_log "Done!"
-
