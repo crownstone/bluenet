8088b1f0e7780c34c0d096abc0a4cdef93963846
vliedel <bart@crownstone.rocks>
2019-04-10 11:12:29 +0200

    Use tabs for indent.

--- include/cs_Crownstone.h
+++ include/cs_Crownstone.h
@@ -42,139 +42,139 @@
 class Crownstone : EventListener {
 public:
 
-    enum ServiceEvent {
-	CREATE_DEVICE_INFO_SERVICE, CREATE_SETUP_SERVICE, CREATE_CROWNSTONE_SERVICE,
-	REMOVE_DEVICE_INFO_SERVICE, REMOVE_SETUP_SERVICE, REMOVE_CROWNSTONE_SERVICE };
+	enum ServiceEvent {
+		CREATE_DEVICE_INFO_SERVICE, CREATE_SETUP_SERVICE, CREATE_CROWNSTONE_SERVICE,
+		REMOVE_DEVICE_INFO_SERVICE, REMOVE_SETUP_SERVICE, REMOVE_CROWNSTONE_SERVICE };
 
-    /**
-     * Constructor.
-     */
-    Crownstone(boards_config_t& board);
+	/**
+	 * Constructor.
+	 */
+	Crownstone(boards_config_t& board);
 
-    /**
-     * Initialize the crownstone:
-     *    1. start UART
-     *    2. configure (drivers, stack, advertisement)
-     *    3. check operation mode
-     *    		3a. for setup mode, create setup services
-     *    		3b. for normal operation mode, create crownstone services
-     *    			and prepare Crownstone for operation
-     *    4. initialize services
-     *
-     * Since some classes initialize asynchronous, this function is called multiple times to continue the process.
-     * When the class is initialized, the init done event will continue the initialization of other classes.
-     */
-    void init(uint16_t step);
+	/**
+	 * Initialize the crownstone:
+	 *    1. start UART
+	 *    2. configure (drivers, stack, advertisement)
+	 *    3. check operation mode
+	 *    		3a. for setup mode, create setup services
+	 *    		3b. for normal operation mode, create crownstone services
+	 *    			and prepare Crownstone for operation
+	 *    4. initialize services
+	 *
+	 * Since some classes initialize asynchronous, this function is called multiple times to continue the process.
+	 * When the class is initialized, the init done event will continue the initialization of other classes.
+	 */
+	void init(uint16_t step);
 
-    /** startup the crownstone:
-     * 	  1. start advertising
-     * 	  2. start processing (mesh, scanner, tracker, tempGuard, etc)
-     */
-    void startUp();
+	/** startup the crownstone:
+	 * 	  1. start advertising
+	 * 	  2. start processing (mesh, scanner, tracker, tempGuard, etc)
+	 */
+	void startUp();
 
-    /** run main
-     *    1. execute app scheduler
-     *    2. wait for ble events
-     */
-    void run();
+	/** run main
+	 *    1. execute app scheduler
+	 *    2. wait for ble events
+	 */
+	void run();
 
-    /** handle (crownstone) events
-    */
-    void handleEvent(event_t & event);
+	/** handle (crownstone) events
+	 */
+	void handleEvent(event_t & event);
 
-    /** tick function called by app timer
-    */
-    static void staticTick(Crownstone *ptr) {
-	ptr->tick();
-    }
+	/** tick function called by app timer
+	 */
+	static void staticTick(Crownstone *ptr) {
+		ptr->tick();
+	}
 
 protected:
 
-    /** initialize drivers (stack, timer, storage, pwm, etc), loads settings from storage.
-    */
-    void initDrivers(uint16_t step);
+	/** initialize drivers (stack, timer, storage, pwm, etc), loads settings from storage.
+	 */
+	void initDrivers(uint16_t step);
 
-    /** configure the crownstone. this will call the other configureXXX functions in turn
-     *    1. configure ble stack
-     *    2. set ble name
-     *    3. configure advertisement
-     *
-     */
-    void configure();
-    void configureStack();
-    void configureAdvertisement();
-    void setName();
+	/** configure the crownstone. this will call the other configureXXX functions in turn
+	 *    1. configure ble stack
+	 *    2. set ble name
+	 *    3. configure advertisement
+	 *
+	 */
+	void configure();
+	void configureStack();
+	void configureAdvertisement();
+	void setName();
 
-    void createService(const ServiceEvent event);
+	void createService(const ServiceEvent event);
 
-    /** Create services available in setup mode
-     */
-    void createSetupServices();
+	/** Create services available in setup mode
+	 */
+	void createSetupServices();
 
-    /** Create services available in normal operation mode
-     */
-    void createCrownstoneServices();
+	/** Create services available in normal operation mode
+	 */
+	void createCrownstoneServices();
 
-    /** Start operation for Crownstone.
-     *
-     * This currently only enables parts in Normal operation mode. This includes:
-     *    - prepare mesh
-     *    - prepare scanner
-     *    - prepare tracker
-     *    - ...
-     */
-    void startOperationMode(const OperationMode & mode);
+	/** Start operation for Crownstone.
+	 *
+	 * This currently only enables parts in Normal operation mode. This includes:
+	 *    - prepare mesh
+	 *    - prepare scanner
+	 *    - prepare tracker
+	 *    - ...
+	 */
+	void startOperationMode(const OperationMode & mode);
 
-    void switchMode(const OperationMode & mode);
+	void switchMode(const OperationMode & mode);
 
-    /** tick function for crownstone to update/execute periodically
-     */
-    void tick();
+	/** tick function for crownstone to update/execute periodically
+	 */
+	void tick();
 
-    /** schedule next execution of tick function
-     */
-    void scheduleNextTick();
+	/** schedule next execution of tick function
+	 */
+	void scheduleNextTick();
 
-    /** Increase reset counter. This will be stored in FLASH so it persists over reboots.
-     */
-    void increaseResetCounter();
+	/** Increase reset counter. This will be stored in FLASH so it persists over reboots.
+	 */
+	void increaseResetCounter();
 private:
 
-    boards_config_t _boardsConfig;
+	boards_config_t _boardsConfig;
 
-    // drivers
-    Stack* _stack;
-    Timer* _timer;
-    Storage* _storage;
-    State* _state;
+	// drivers
+	Stack* _stack;
+	Timer* _timer;
+	Storage* _storage;
+	State* _state;
 
-    Switch* _switch;
-    TemperatureGuard* _temperatureGuard;
-    PowerSampling* _powerSampler;
-    Watchdog* _watchdog;
+	Switch* _switch;
+	TemperatureGuard* _temperatureGuard;
+	PowerSampling* _powerSampler;
+	Watchdog* _watchdog;
 
-    // services
-    DeviceInformationService* _deviceInformationService;
-    CrownstoneService* _crownstoneService;
-    SetupService* _setupService;
+	// services
+	DeviceInformationService* _deviceInformationService;
+	CrownstoneService* _crownstoneService;
+	SetupService* _setupService;
 
-    // advertise
-    ServiceData* _serviceData;
-    IBeacon* _beacon;
+	// advertise
+	ServiceData* _serviceData;
+	IBeacon* _beacon;
 
-    // processing
+	// processing
 #if BUILD_MESHING == 1
-    Mesh* _mesh;
+	Mesh* _mesh;
 #endif
-    CommandHandler* _commandHandler;
-    Scanner* _scanner;
-    Scheduler* _scheduler;
-    FactoryReset* _factoryReset;
+	CommandHandler* _commandHandler;
+	Scanner* _scanner;
+	Scheduler* _scheduler;
+	FactoryReset* _factoryReset;
 
-    app_timer_t              _mainTimerData;
-    app_timer_id_t           _mainTimerId;
+	app_timer_t              _mainTimerData;
+	app_timer_id_t           _mainTimerId;
 
-    OperationMode _operationMode;
+	OperationMode _operationMode;
 
 };
 
