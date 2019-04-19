4677f04529b1b738e213771daf3f645d759c1aea
vliedel <bart@crownstone.rocks>
2019-04-19 15:04:45 +0200

    add color to nrf logs

--- include/third/nrf/sdk_config.h
+++ include/third/nrf/sdk_config.h
@@ -8233,7 +8233,7 @@
 // <e> NRF_LOG_USES_COLORS - If enabled then ANSI escape code for colors is prefixed to every string
 //==========================================================
 #ifndef NRF_LOG_USES_COLORS
-#define NRF_LOG_USES_COLORS 0
+#define NRF_LOG_USES_COLORS 1
 #endif
 // <o> NRF_LOG_COLOR_DEFAULT  - ANSI escape code prefix.
 
