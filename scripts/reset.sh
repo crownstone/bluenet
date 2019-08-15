cb0138cc5df55639e52779f5043909400790767a
vliedel <bart@crownstone.rocks>
2019-08-15 11:41:48 +0200

    Use target as argument .

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-SERIAL_NUM=$1
+# optional target, use crownstone as default
+target=${1:-crownstone}
 
+# Get the scripts path: the path where this file is located.
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $path/_utils.sh
+
+cs_info "Load configuration from: ${path}/_check_targets.sh $target"
+source ${path}/_check_targets.sh $target
+
+cs_info "Load configuration from: ${path}/_config.sh"
 source $path/_config.sh
 
 cs_info "resetting device"
-if [ $SERIAL_NUM ]; then
-	nrfjprog -f nrf52 --reset --snr $SERIAL_NUM
+if [ $serial_num ]; then
+	nrfjprog -f nrf52 --reset --snr $serial_num
 else
 	nrfjprog -f nrf52 --reset
 fi
