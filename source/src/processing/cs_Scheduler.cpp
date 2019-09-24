96bb8261d1b9c3deaa0612b533f06f0ead5372b3
Arend de Jonge <arenddejonge@gmail.com>
2019-09-24 14:17:35 +0200

    remove time related functionality from Scheduler

--- source/src/processing/cs_Scheduler.cpp
+++ source/src/processing/cs_Scheduler.cpp
@@ -7,23 +7,19 @@
 
 #include "common/cs_Types.h"
 #include "drivers/cs_Serial.h"
-#include "drivers/cs_RTC.h"
+
 #include "events/cs_EventDispatcher.h"
 #include "processing/cs_Scheduler.h"
 #include "processing/cs_Switch.h"
 #include "storage/cs_State.h"
+#include "util/cs_SystemTime.h"
 
 //#define SCHEDULER_PRINT_DEBUG
 
 Scheduler::Scheduler() :
-//	EventListener(EVT_ALL),
-	_appTimerId(NULL),
-	_rtcTimeStamp(0),
-	_posixTimeStamp(0),
 	_scheduleList(NULL)
 {
-	_appTimerData = { {0} };
-	_appTimerId = &_appTimerData;
+
 
 	_scheduleList = new ScheduleList();
 	_scheduleList->assign(_schedulerBuffer, sizeof(_schedulerBuffer));
@@ -33,24 +29,28 @@ Scheduler::Scheduler() :
 
 	// Subscribe for events.
 	EventDispatcher::getInstance().addListener(this);
-
-	// Init and start the timer.
-	Timer::getInstance().createSingleShot(_appTimerId, (app_timer_timeout_handler_t) Scheduler::staticTick);
-	scheduleNextTick();
 }
 
-
-// TODO: move time / set time to separate class
-void Scheduler::setTime(uint32_t time) {
-	LOGi("Set time to %i", time);
-	if (time == 0) {
-		return;
+void Scheduler::handleEvent(event_t & event){
+	switch(event.type){
+		case CS_TYPE::STATE_TIME:{
+			processScheduledAction();
+		}
+		case CS_TYPE::EVT_TIME_SET:{
+			// TODO(Arend): add diff parameter to EVT_TIME_SET
+			uint64_t diff = 0;
+			handleSetTimeEvent(diff);
+		}
+		default:{
+			break;
+		}
 	}
+}
 
-	// Update time
-	int64_t diff = time - _posixTimeStamp;
-	_posixTimeStamp = time;
-	_rtcTimeStamp = RTC::getCount();
+// ================= ScheduleList administration ================== 
+
+void Scheduler::handleSetTimeEvent(uint64_t diff) {
+	uint32_t time = SystemTime::posix();
 
 	// If there is a time jump: sync the entries
 	printDebug();
@@ -59,52 +59,11 @@ void Scheduler::setTime(uint32_t time) {
 		writeScheduleList(true);
 	}
 	printDebug();
-	event_t event(CS_TYPE::EVT_TIME_SET);
-	EventDispatcher::getInstance().dispatch(event);
 }
 
-cs_ret_code_t Scheduler::setScheduleEntry(uint8_t id, schedule_entry_t* entry) {
-	LOGd("set %u", id);
-//	if (entry->nextTimestamp == 0) {
-//		return clearScheduleEntry(id);
-//	}
-	if (entry->nextTimestamp <= _posixTimeStamp) {
-		return ERR_WRONG_PARAMETER;
-	}
-	if (!_scheduleList->set(id, entry)) {
-		return ERR_WRONG_PARAMETER;
-	}
-	writeScheduleList(true);
-	printDebug();
-	publishScheduleEntries();
-	return ERR_SUCCESS;
-}
+void Scheduler::processScheduledAction(){
+	schedule_entry_t* entry = _scheduleList->isActionTime(SystemTime::posix());
 
-cs_ret_code_t Scheduler::clearScheduleEntry(uint8_t id) {
-	LOGd("clear %u", id);
-	if (!_scheduleList->clear(id)) {
-		return ERR_WRONG_PARAMETER;
-	}
-	writeScheduleList(true);
-	printDebug();
-	publishScheduleEntries();
-	return ERR_SUCCESS;
-}
-
-void Scheduler::tick() {
-	// RTC can overflow every 512s
-	uint32_t tickDiff = RTC::difference(RTC::getCount(), _rtcTimeStamp);
-
-	// If more than 1s elapsed since last set rtc timestamp:
-	// add 1s to posix time and subtract 1s from tickDiff, by increasing the rtc timestamp 1s
-	if (_posixTimeStamp && tickDiff > RTC::msToTicks(1000)) {
-		_posixTimeStamp++;
-		_rtcTimeStamp += RTC::msToTicks(1000);
-
-		State::getInstance().set(CS_TYPE::STATE_TIME, &_posixTimeStamp, sizeof(_posixTimeStamp));
-	}
-
-	schedule_entry_t* entry = _scheduleList->isActionTime(_posixTimeStamp);
 	if (entry != NULL) {
 		switch (ScheduleEntry::getActionType(entry)) {
 			case SCHEDULE_ACTION_TYPE_PWM: {
@@ -128,8 +87,34 @@ void Scheduler::tick() {
 		}
 		writeScheduleList(false);
 	}
+}
 
-	scheduleNextTick();
+cs_ret_code_t Scheduler::setScheduleEntry(uint8_t id, schedule_entry_t* entry) {
+	LOGd("set %u", id);
+//	if (entry->nextTimestamp == 0) {
+//		return clearScheduleEntry(id);
+//	}
+	if (entry->nextTimestamp <= SystemTime::posix()) {
+		return ERR_WRONG_PARAMETER;
+	}
+	if (!_scheduleList->set(id, entry)) {
+		return ERR_WRONG_PARAMETER;
+	}
+	writeScheduleList(true);
+	printDebug();
+	publishScheduleEntries();
+	return ERR_SUCCESS;
+}
+
+cs_ret_code_t Scheduler::clearScheduleEntry(uint8_t id) {
+	LOGd("clear %u", id);
+	if (!_scheduleList->clear(id)) {
+		return ERR_WRONG_PARAMETER;
+	}
+	writeScheduleList(true);
+	printDebug();
+	publishScheduleEntries();
+	return ERR_SUCCESS;
 }
 
 void Scheduler::writeScheduleList(bool store) {
@@ -173,32 +158,7 @@ void Scheduler::publishScheduleEntries() {
 	EventDispatcher::getInstance().dispatch(event);
 }
 
-void Scheduler::handleEvent(event_t & event) {
-	switch(event.type) {
-		case CS_TYPE::STATE_TIME: {
-			// Time was set via State.set().
-			// This may have been set by us! So only use it when no time is set yet?
-			if (_posixTimeStamp == 0 && event.size == sizeof(TYPIFY(STATE_TIME))) {
-				setTime(*((TYPIFY(STATE_TIME)*)event.data));
-			}
-			break;
-		}
-		case CS_TYPE::EVT_MESH_TIME: {
-			// Only set the time if there is currently no time set, as these timestamps may be old
-			if (_posixTimeStamp == 0 && event.size == sizeof(TYPIFY(EVT_MESH_TIME))) {
-				setTime(*((TYPIFY(EVT_MESH_TIME)*)event.data));
-			}
-			break;
-		}
-		case CS_TYPE::CMD_SET_TIME: {
-			if (event.size == sizeof(TYPIFY(CMD_SET_TIME))) {
-				setTime(*((TYPIFY(CMD_SET_TIME)*)event.data));
-			}
-			break;
-		}
-		default: {}
-	}
-}
+// ============== DEBUG =============
 
 void Scheduler::print() {
 	_scheduleList->print();
