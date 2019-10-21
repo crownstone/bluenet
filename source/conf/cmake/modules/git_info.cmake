47a4bc0dd9371155eefd475db162ebfbecc7647e
Anne van Rossum <anne@crownstone.rocks>
2019-10-21 12:34:53 +0200

    Add VERBOSE=1 make to travis

--- source/conf/cmake/modules/git_info.cmake
+++ source/conf/cmake/modules/git_info.cmake
@@ -1,8 +1,8 @@
 function(git_info GIT_PATH GIT_BRANCH GIT_HASH)
   if(EXISTS ${GIT_PATH})
     # Note, these commands are Linux only. Check for other OS how to retrieve this info
-    execute_process(COMMAND git rev-parse --abbrev-ref HEAD WORKING_DIRECTORY ${GIT_PATH} OUTPUT_VARIABLE BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process(COMMAND git rev-parse --short=24 HEAD WORKING_DIRECTORY ${GIT_PATH} OUTPUT_VARIABLE HASH OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process(COMMAND git rev-parse --abbrev-ref HEAD WORKING_DIRECTORY "${GIT_PATH}" OUTPUT_VARIABLE BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process(COMMAND git rev-parse --short=24 HEAD WORKING_DIRECTORY "${GIT_PATH}" OUTPUT_VARIABLE HASH OUTPUT_STRIP_TRAILING_WHITESPACE)
     set(${GIT_BRANCH} "${BRANCH}" PARENT_SCOPE)
     set(${GIT_HASH} "${HASH}" PARENT_SCOPE)
   else()
