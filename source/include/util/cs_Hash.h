661c90d98e4758c0deca2735eaa5d971fcf03c81
Arend de Jonge <arenddejonge@gmail.com>
2019-11-06 12:29:09 +0100

    comments added

--- source/include/util/cs_Hash.h
+++ source/include/util/cs_Hash.h
@@ -16,17 +16,19 @@
  * As the Fletcher32 hash is effectively a uint16_t sized block stream hash, the computation
  * of a hash of large amounts of data can be split.
  * 
+ * Warning: this only holds as long as the lengths of intermediate 
+ * data chunks are a multiple of 2.
+ * 
  * E.g.
  * 
- * uint8_t part0[] = { __data__ }; size_t len0;
- * uint8_t part1[] = { __data__ }; size_t len1;
- * uint8_t part2[] = { __data__ }; size_t len2;
+ * uint8_t part0[] = { __data__ }; size_t len0; // must be 0 % 2
+ * uint8_t part1[] = { __data__ }; size_t len1; // must be 0 % 2
+ * uint8_t part2[] = { __data__ }; size_t len2; // may be 1 % 2
  * 
  * uint32_t fletch;
  * fletch = Fletcher(part0,len0);
  * fletch = Fletcher(part1,len1,fletch);
  * fletch = Fletcher(part2,len2,fletch);
- * ...
  *  
  */
 uint32_t Fletcher(uint8_t* data, const size_t len, uint32_t previousFletcherHash = 0);
