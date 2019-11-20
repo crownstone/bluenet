dec2ac1d7581a79bbb0d622cceccb67d49843b97
vliedel <bart@crownstone.rocks>
2019-11-20 16:59:21 +0100

    Actually respect the t2t enabled setting.

--- source/src/processing/cs_TapToToggle.cpp
+++ source/src/processing/cs_TapToToggle.cpp
@@ -32,6 +32,9 @@ void TapToToggle::init(int8_t defaultRssiThreshold) {
 
 void TapToToggle::handleBackgroundAdvertisement(adv_background_parsed_t* adv) {
 	LOGT2Td("addr=%x:%x:%x:%x:%x:%x rssi=%i flags=%u", adv->macAddress[0], adv->macAddress[1], adv->macAddress[2], adv->macAddress[3], adv->macAddress[4], adv->macAddress[5], adv->adjustedRssi, adv->flags);
+	if (!enabled) {
+		return;
+	}
 	if (adv->adjustedRssi < rssiThreshold) {
 		LOGT2Td("rssi too low");
 		return;
