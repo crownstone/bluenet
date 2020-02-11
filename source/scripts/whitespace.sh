61d194c6ee0aa100736e74577305bbca3539ed29
Anne van Rossum <anne@crownstone.rocks>
2020-02-11 18:21:41 +0100

    Whitespace expression that is multi-line comment compatible.

--- source/scripts/whitespace.sh
+++ source/scripts/whitespace.sh
@@ -2,5 +2,5 @@
 
 path=${1:? "Usage: $0 path"}
 
-find $path -regex '.*\.\(c\|h\|cpp\|hpp\)' -print0 | xargs -0 grep -P "^(\s+ +)|( +    +\s*)" 
+find $path -regex '.*\.\(c\|h\|cpp\|hpp\)' -print0 | xargs -0 grep -P '^(\s+ +(?!\*))|( +\t+\s*)'
 
