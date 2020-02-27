b9ecfd525b97d85f0167c4f2c36391dada0b1c61
vliedel <bart@crownstone.rocks>
2020-02-27 17:17:06 +0100

    speed up printUicr

--- source/scripts/printUicr.sh
+++ source/scripts/printUicr.sh
@@ -6,12 +6,10 @@ if [ $# -lt 0 ]; then
 fi
 
 if [ $# -lt 1 ]; then
-        nrfjprog --memrd 0x10001000 --n 0x80
-        nrfjprog --memrd 0x10001080 --n 0x80
+        nrfjprog --memrd 0x10001000 --n 0x100
         nrfjprog --reset
 else
-        nrfjprog --memrd 0x10001000 --n 0x80 -s $1
-        nrfjprog --memrd 0x10001080 --n 0x80 -s $1
+        nrfjprog --memrd 0x10001000 --n 0x100 -s $1
         nrfjprog --reset -s $1
 fi
 
