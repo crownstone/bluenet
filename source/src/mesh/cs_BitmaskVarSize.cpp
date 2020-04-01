63e73f1eecdace322313850172cc73a351d02771
vliedel <bart@crownstone.rocks>
2020-04-01 17:25:49 +0200

    Added variable sized bitmask class.

---
+++ source/src/mesh/cs_BitmaskVarSize.cpp
@@ -0,0 +1,84 @@
+/*
+ * Author: Crownstone Team
+ * Copyright: Crownstone (https://crownstone.rocks)
+ * Date: Apr 1, 2020
+ * License: LGPLv3+, Apache License 2.0, and/or MIT (triple-licensed)
+ */
+
+#include <mesh/cs_BitmaskVarSize.h>
+#include <cstdlib>
+#include <cstring>
+
+
+BitmaskVarSize::BitmaskVarSize() {
+
+}
+
+BitmaskVarSize::~BitmaskVarSize() {
+	if (_bitmask != nullptr) {
+		free(_bitmask);
+	}
+}
+
+bool BitmaskVarSize::setNumBits(uint8_t numBits) {
+	if (_bitmask != nullptr) {
+		free(_bitmask);
+	}
+	_bitmask = (uint8_t*)malloc(getNumBytes(numBits));
+	if (_bitmask == nullptr) {
+		_numBits = 0;
+		return false;
+	}
+	_numBits = numBits;
+	return true;
+}
+
+bool BitmaskVarSize::setBit(uint8_t bit) {
+	if (bit >= _numBits) {
+		return false;
+	}
+	_bitmask[bit / 8] |= (1 << (bit % 8));
+	return true;
+}
+
+bool BitmaskVarSize::clearBit(uint8_t bit) {
+	if (bit >= _numBits) {
+		return false;
+	}
+	_bitmask[bit / 8] &= ~(1 << (bit % 8));
+	return true;
+}
+
+bool BitmaskVarSize::isSet(uint8_t bit) {
+	if (bit >= _numBits) {
+		return false;
+	}
+	return _bitmask[bit / 8] & (1 << (bit % 8));
+}
+
+void BitmaskVarSize::clearAllBits() {
+	memset(_bitmask, 0, getNumBytes(_numBits));
+}
+
+bool BitmaskVarSize::isAllBitsSet() {
+	// First check all bytes that should have all 8 bits set.
+	uint8_t numFullBytes = _numBits / 8;
+	for (uint8_t i=0; i < numFullBytes; ++i) {
+		if (_bitmask[i] != 0xFF) {
+			return false;
+		}
+	}
+
+	// If any bits are remaining, the last byte should match a bitmask with all those bits set.
+	uint8_t numRemainingBits = _numBits % 8;
+	uint8_t remainingBitmask = (1 << numRemainingBits) - 1;
+	if (numRemainingBits && (_bitmask[numFullBytes] & remainingBitmask) != remainingBitmask) {
+		return false;
+	}
+	return true;
+}
+
+uint8_t BitmaskVarSize::getNumBytes(uint8_t numBits) {
+	return (numBits + 7) / 8; // 8 bits per byte, rounded up.
+}
+
