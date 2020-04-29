9c0fb305a8d7585d41c69c4fc5ceace45d130271
Arend de Jonge <arenddejonge@gmail.com>
2020-04-29 14:37:35 +0200

    clean up the comparrison

--- source/src/behaviour/cs_TwilightHandler.cpp
+++ source/src/behaviour/cs_TwilightHandler.cpp
@@ -21,7 +21,7 @@ bool FromUntilIntervalIsMoreRelevantOrEqual(
 		int32_t current_tod){
 	constexpr uint32_t seconds_per_day = 24*60*60;
 
-	// Note: I think the current_tod actually isn't necessary: in practice,
+	// Note: I think the current_tod parameter actually isn't necessary:
 	// we only compare behavious when they are both active, meaning that current_tod
 	// is contained in the intersection of the intervals. All values satisfying that
 	// restriction seem to have the same return value. (Needs to be rigidly proven I guess..)
@@ -29,43 +29,30 @@ bool FromUntilIntervalIsMoreRelevantOrEqual(
 	// If this would be the case the parameter current_tod can be taken any value in this
 	// intersection which simplifies the model significantly.
 
-	// be careful about integer underflow in the subtraction. Changed the signature to _signed_
-	// integers in order to circumvent extra casting here.
+	// First we normalize the from/until times so that current_tod corresponds to '0'.
+	// That way, comparisons like lhs_from < rhs_from do not suffer from subtleties concerning
+	// midnight roll over and unsigned integer over/underflow.
+	//
+	// Warning: be careful about integer underflow in the subtraction. Changed the signature to
+	// _signed_ integers in order to circumvent extra casting here.
 	uint32_t lhs_from_normalized = CsMath::mod(lhs_from - current_tod, seconds_per_day);
 	uint32_t rhs_from_normalized = CsMath::mod(rhs_from - current_tod, seconds_per_day);
-
 	uint32_t lhs_until_normalized = CsMath::mod(lhs_until - current_tod, seconds_per_day);
 	uint32_t rhs_until_normalized = CsMath::mod(rhs_until - current_tod, seconds_per_day);
 
 	if ( lhs_from_normalized > rhs_from_normalized ) {
-			 // Essentially "lhs.from() > rhs.from()" means lhs.from is less far in the past.
-			 // However, this equation only works as expected when both from() values are on the
-			 // same day. The subtleties of midnight-rollover must be resolved by subtracting
-			 // currenttime and modding by seconds_per_day before comparison.
+			 // essentially means lhs_from is less far in the past than rhs_from.
 
-//			LOGd("lhs is more specific: from() is less far in the past: lhs.f (%d) > rhs.f (%d) at time %d",
-////					lhs_from_normalized, rhs_from_normalized, current_tod);
-//					lhs_from, rhs_from, current_tod);
 			return true;
 	}
 
 	if( rhs_from_normalized == lhs_from_normalized &&
 		lhs_until_normalized <=	rhs_until_normalized){
-		// play the same game on until() times when the from times are identical.
-		// The until time that is nearest in the future should win now, so
-		// the comparison sign reverses. Be careful about the 'or equal' part too.
-
-//		LOGd("lhs is more specific: from()s are equal and until() is less far in the future: lhs.u (%d) <= rhs.u(%d) at %d",
-//				//lhs_until_normalized, rhs_until_normalized, current_tod);
-//				lhs_until, rhs_until, current_tod);
+		// essentially means lhs_until is nearer in the future than rhs_until.
+		// (Be careful about the 'or equal' part.)
 		return true;
 	}
-//
-//	LOGd("FromUntilIntervalIsMoreRelevantOrEqual returns false, lhs/current [%d - %d], rhs/prevwinn [%d - %d]",
-//			lhs_from_normalized, lhs_until_normalized,rhs_from_normalized, rhs_until_normalized);
-//	LOGd("FromUntilIntervalIsMoreRelevantOrEqual input params, lhs/current [%d - %d], rhs/prevwinn [%d - %d]",
-//				lhs_from, lhs_until,rhs_from, rhs_until);
-//	LOGd("FromUntilIntervalIsMoreRelevantOrEqual current_tod %d", current_tod);
+
 	return false;
 }
 
@@ -118,9 +105,6 @@ std::optional<uint8_t> TwilightHandler::computeIntendedState(Time currenttime){
     uint32_t winning_until_tod = 0;
     uint8_t winning_value = 0xff;
 
-    uint8_t winning_index = 0xff;
-    uint8_t current_index = 0x0;
-
     // loop through all twilight behaviours searching for valid ones.
     for (auto& b : BehaviourStore::getActiveBehaviours()) {
         if (TwilightBehaviour * behave = dynamic_cast<TwilightBehaviour*>(b)) {
@@ -145,14 +129,10 @@ std::optional<uint8_t> TwilightHandler::computeIntendedState(Time currenttime){
             	}
 
 				if(ok_to_overwrite_winning_value){
-					LOGd("Twilight conflict res: overwriting winning value (index %d is better than %d) at %02d:%02d h",
-							current_index, winning_index, currenttime.timeOfDay().h(), currenttime.timeOfDay().m());
-
 					// what a silly edge case, there are two (or more) twilights with the exact same
 					// boundary times. Let's use the minimum value in that case.
 					if(candidate_from_tod == winning_from_tod &&
 							candidate_until_tod == winning_until_tod){
-						LOGd("exact interal match, using minimum twilight value");
 						winning_value = CsMath::min(behave->value(), winning_value);
 					} else {
 						winning_value = behave->value();
@@ -161,13 +141,9 @@ std::optional<uint8_t> TwilightHandler::computeIntendedState(Time currenttime){
 					// update winning value and winning from/until boundaries
 					winning_from_tod = candidate_from_tod;
 					winning_until_tod = candidate_until_tod;
-
-					winning_index = current_index;
 				}
             }
         }
-
-        current_index++;
     }
 
     // if no winning_value is found at all, return 100.
