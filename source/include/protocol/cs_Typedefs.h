37a18e548bfe6fc10b91de4081b17c37a8e4d287
vliedel <bart@crownstone.rocks>
2020-09-18 14:26:11 +0200

    Note stone id 0 is invalid.

--- source/include/protocol/cs_Typedefs.h
+++ source/include/protocol/cs_Typedefs.h
@@ -14,7 +14,7 @@ typedef uint8_t* buffer_ptr_t;
 typedef uint16_t cs_buffer_size_t;
 typedef uint16_t cs_ret_code_t;
 typedef uint16_t cs_control_cmd_t;
-typedef uint8_t  stone_id_t;
+typedef uint8_t  stone_id_t; // Stone ID 0 is invalid.
 typedef uint16_t device_id_t;
 typedef uint16_t size16_t;
 //! Boolean with fixed size.
