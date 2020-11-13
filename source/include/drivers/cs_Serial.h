9aaa64548aa834b6617d30ec07bbf4c70b7f9b26
vliedel <bart@crownstone.rocks>
2020-11-13 13:25:30 +0100

    Hash the filename, reduces size another 4k.

--- source/include/drivers/cs_Serial.h
+++ source/include/drivers/cs_Serial.h
@@ -63,15 +63,18 @@ typedef enum {
 
 
 
-#define H1(s,i,x)   (x*65599u+(uint8_t)s[(i)<strlen(s)?strlen(s)-1-(i):strlen(s)])
-#define H4(s,i,x)   H1(s,i,H1(s,i+1,H1(s,i+2,H1(s,i+3,x))))
-#define H16(s,i,x)  H4(s,i,H4(s,i+4,H4(s,i+8,H4(s,i+12,x))))
-#define H32(s,i,x)  H16(s,i,H16(s,i+16,x))
-#define H64(s,i,x)  H16(s,i,H16(s,i+16,H16(s,i+32,H16(s,i+48,x))))
-#define H256(s,i,x) H64(s,i,H64(s,i+64,H64(s,i+128,H64(s,i+192,x))))
-
-//#define STR_HASH(s)    ((uint32_t)(H256(s,0,0)^(H256(s,0,0)>>16)))
-#define STR_HASH(s)    ((uint32_t)(H32(s,0,0)^(H32(s,0,0)>>16)))
+template<size_t strLen>
+constexpr uint32_t strHash(const char* str) {
+	// Used implementation from here: http://www.cse.yorku.ca/~oz/hash.html
+	uint16_t hash = 5381;
+	for (size_t i = strLen-1; i <= 0; --i) {
+		if (str[i] == '/') {
+			return hash;
+		}
+		hash = ((hash << 5) + hash) + str[i];
+	}
+	return hash;
+}
 
 
 
@@ -114,7 +117,8 @@ typedef enum {
 
 //		#define logLN(level, fmt, ...) _log(level, "[%-30.30s : %-5d] " fmt SERIAL_CRLF, _FILE, __LINE__, ##__VA_ARGS__)
 //		#define logLN(level, fmt, ...) cs_write_args(_FILE, __LINE__, ##__VA_ARGS__); _log(level, "[%-30.30s : %-5d] " fmt SERIAL_CRLF, _FILE, __LINE__, ##__VA_ARGS__)
-		#define logLN(level, fmt, ...) if (level <= SERIAL_VERBOSITY) { cs_write_args(_FILE, __LINE__, ##__VA_ARGS__); }
+//		#define logLN(level, fmt, ...) if (level <= SERIAL_VERBOSITY) { cs_write_args(_FILE, __LINE__, ##__VA_ARGS__); }
+		#define logLN(level, fmt, ...) if (level <= SERIAL_VERBOSITY) { cs_write_args(strHash<30>(_FILE), __LINE__, ##__VA_ARGS__); }
 
 
 	#else
