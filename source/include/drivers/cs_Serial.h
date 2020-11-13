6d3b5d0a5c9ebce07032bd51d056de9b7bc750ab
vliedel <bart@crownstone.rocks>
2020-11-13 10:22:35 +0100

    Started trying out binary logs.

--- source/include/drivers/cs_Serial.h
+++ source/include/drivers/cs_Serial.h
@@ -6,15 +6,16 @@
  */
 #pragma once
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+//#ifdef __cplusplus
+//extern "C" {
+//#endif
 
 #include <cfg/cs_Debug.h>
 #include <cfg/cs_Git.h>
 #include <ble/cs_Nordic.h>
 #include <cfg/cs_Strings.h>
 
+#include <type_traits>
 #include "stdint.h"
 #include "cstring"
 
@@ -65,51 +66,58 @@ typedef enum {
 #define LOGnone LOG_IGNORE
 
 #if !defined HOST_TARGET && (CS_SERIAL_NRF_LOG_ENABLED > 0)
-#define LOG_FLUSH NRF_LOG_FLUSH
+	#define LOG_FLUSH NRF_LOG_FLUSH
 #else
-#define LOG_NOTHING()
-#define LOG_FLUSH LOG_NOTHING
+	#define LOG_NOTHING()
+	#define LOG_FLUSH LOG_NOTHING
 #endif
 
 #if !defined HOST_TARGET && (CS_SERIAL_NRF_LOG_ENABLED == 2)
-//#warning NRF_LOG
-#define LOGv NRF_LOG_DEBUG
-#define LOGd NRF_LOG_DEBUG
-#define LOGi NRF_LOG_INFO
-#define LOGw NRF_LOG_WARNING
-#define LOGe NRF_LOG_ERROR
-#define LOGf NRF_LOG_ERROR
-#define _log(level, fmt, ...)
-#define logLN(level, fmt, ...)
+	#ifdef __cplusplus
+	extern "C" {
+	#endif
+		//#warning NRF_LOG
+		#define LOGv NRF_LOG_DEBUG
+		#define LOGd NRF_LOG_DEBUG
+		#define LOGi NRF_LOG_INFO
+		#define LOGw NRF_LOG_WARNING
+		#define LOGe NRF_LOG_ERROR
+		#define LOGf NRF_LOG_ERROR
+		#define _log(level, fmt, ...)
+		#define logLN(level, fmt, ...)
+	#ifdef __cplusplus
+	}
+	#endif
 #else
 
-#if SERIAL_VERBOSITY > SERIAL_BYTE_PROTOCOL_ONLY
+	#if SERIAL_VERBOSITY > SERIAL_BYTE_PROTOCOL_ONLY
 
-#include "string.h"
-//	#define _FILE (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
-//  #define STRING_BACK(s, l) (sizeof(s) > l ? s + (sizeof(s)-l-1) : s)
-#define _FILE (sizeof(__FILE__) > 30 ? __FILE__ + (sizeof(__FILE__)-30-1) : __FILE__)
+		#include "string.h"
+		//	#define _FILE (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+		//  #define STRING_BACK(s, l) (sizeof(s) > l ? s + (sizeof(s)-l-1) : s)
+		#define _FILE (sizeof(__FILE__) > 30 ? __FILE__ + (sizeof(__FILE__)-30-1) : __FILE__)
 
-#define _log(level, fmt, ...) \
-		if (level <= SERIAL_VERBOSITY) { \
-			cs_write(fmt, ##__VA_ARGS__); \
-		}
+		#define _log(level, fmt, ...) \
+				if (level <= SERIAL_VERBOSITY) { \
+					cs_write(fmt, ##__VA_ARGS__); \
+				}
 
-#define logLN(level, fmt, ...) \
-		_log(level, "[%-30.30s : %-5d] " fmt SERIAL_CRLF, _FILE, __LINE__, ##__VA_ARGS__)
+		#define logLN(level, fmt, ...) _log(level, "[%-30.30s : %-5d] " fmt SERIAL_CRLF, _FILE, __LINE__, ##__VA_ARGS__)
+//		#define logLN(level, fmt, ...) cs_write_args(_FILE, __LINE__, ##__VA_ARGS__); _log(level, "[%-30.30s : %-5d] " fmt SERIAL_CRLF, _FILE, __LINE__, ##__VA_ARGS__)
+//		#define logLN(level, fmt, ...) if (level <= SERIAL_VERBOSITY) { cs_write_args(_FILE, __LINE__, ##__VA_ARGS__); }
 
 
-#else
-#define _log(level, fmt, ...)
-#define logLN(level, fmt, ...)
-#endif
+	#else
+		#define _log(level, fmt, ...)
+		#define logLN(level, fmt, ...)
+	#endif
 
-#define LOGv(fmt, ...) logLN(SERIAL_VERBOSE, "\033[37;1m" fmt "\033[0m", ##__VA_ARGS__) // White
-#define LOGd(fmt, ...) logLN(SERIAL_DEBUG,   "\033[37;1m" fmt "\033[0m", ##__VA_ARGS__) // White
-#define LOGi(fmt, ...) logLN(SERIAL_INFO,    "\033[34;1m" fmt "\033[0m", ##__VA_ARGS__) // Blue
-#define LOGw(fmt, ...) logLN(SERIAL_WARN,    "\033[33;1m" fmt "\033[0m", ##__VA_ARGS__) // Yellow
-#define LOGe(fmt, ...) logLN(SERIAL_ERROR,   "\033[35;1m" fmt "\033[0m", ##__VA_ARGS__) // Purple
-#define LOGf(fmt, ...) logLN(SERIAL_FATAL,   "\033[31;1m" fmt "\033[0m", ##__VA_ARGS__) // Red
+	#define LOGv(fmt, ...) logLN(SERIAL_VERBOSE, "\033[37;1m" fmt "\033[0m", ##__VA_ARGS__) // White
+	#define LOGd(fmt, ...) logLN(SERIAL_DEBUG,   "\033[37;1m" fmt "\033[0m", ##__VA_ARGS__) // White
+	#define LOGi(fmt, ...) logLN(SERIAL_INFO,    "\033[34;1m" fmt "\033[0m", ##__VA_ARGS__) // Blue
+	#define LOGw(fmt, ...) logLN(SERIAL_WARN,    "\033[33;1m" fmt "\033[0m", ##__VA_ARGS__) // Yellow
+	#define LOGe(fmt, ...) logLN(SERIAL_ERROR,   "\033[35;1m" fmt "\033[0m", ##__VA_ARGS__) // Purple
+	#define LOGf(fmt, ...) logLN(SERIAL_FATAL,   "\033[31;1m" fmt "\033[0m", ##__VA_ARGS__) // Red
 #endif
 
 #define LOG_MEMORY \
@@ -120,33 +128,33 @@ typedef enum {
 	free(p);
 
 #if SERIAL_VERBOSITY < SERIAL_VERBOSE
-#undef LOGv
-#define LOGv(fmt, ...)
+	#undef LOGv
+	#define LOGv(fmt, ...)
 #endif
 
 #if SERIAL_VERBOSITY < SERIAL_DEBUG
-#undef LOGd
-#define LOGd(fmt, ...)
+	#undef LOGd
+	#define LOGd(fmt, ...)
 #endif
 
 #if SERIAL_VERBOSITY < SERIAL_INFO
-#undef LOGi
-#define LOGi(fmt, ...)
+	#undef LOGi
+	#define LOGi(fmt, ...)
 #endif
 
 #if SERIAL_VERBOSITY < SERIAL_WARN
-#undef LOGw
-#define LOGw(fmt, ...)
+	#undef LOGw
+	#define LOGw(fmt, ...)
 #endif
 
 #if SERIAL_VERBOSITY < SERIAL_ERROR
-#undef LOGe
-#define LOGe(fmt, ...)
+	#undef LOGe
+	#define LOGe(fmt, ...)
 #endif
 
 #if SERIAL_VERBOSITY < SERIAL_FATAL
-#undef LOGf
-#define LOGf(fmt, ...)
+	#undef LOGf
+	#define LOGf(fmt, ...)
 #endif
 
 /**
@@ -181,9 +189,9 @@ bool serial_tx_ready();
  * Write a string with printf functionality.
  */
 #ifdef HOST_TARGET
-#define cs_write printf
+	#define cs_write printf
 #else
-int cs_write(const char *str, ...);
+	int cs_write(const char *str, ...);
 #endif
 
 /**
@@ -191,6 +199,134 @@ int cs_write(const char *str, ...);
  */
 void writeByte(uint8_t val);
 
-#ifdef __cplusplus
+//template<class T, typename = void>
+//void cs_write_val(const T& val) {
+//	cs_write("void[]");
+//}
+//
+//template<class T>
+//void cs_write_val<T, typename std::enable_if<std::is_same< T, const char* >::value>::type>(const char *str) {
+//	cs_write("str(len=%u)[", strlen(str));
+//	for (unsigned int i = 0; i < strlen(str); ++i) {
+//		cs_write("%u ", str[i]);
+//	}
+//	cs_write("] ");
+//}
+//
+//template<class T>
+//void cs_write_val<T, typename std::enable_if<std::is_arithmetic<T>::value >::type>(const T& val) {
+//	const volatile uint8_t* const valPtr = reinterpret_cast<const volatile uint8_t* const>(&val);
+//	cs_write("val[");
+//	for (unsigned int i = 0; i < sizeof(T); ++i) {
+//		cs_write("%u ", valPtr[i]);
+//	}
+//	cs_write("] ");
+//}
+
+
+
+
+
+
+template<class T>
+void cs_write_val(const T val) {
+	const volatile uint8_t* const valPtr = reinterpret_cast<const volatile uint8_t* const>(&val);
+	cs_write("val[");
+	for (unsigned int i = 0; i < sizeof(T); ++i) {
+		cs_write("%u ", valPtr[i]);
+	}
+	cs_write("] ");
 }
-#endif
+
+//typedef char* cs_char_ptr_t;
+//
+//template<>
+//void cs_write_val(const cs_char_ptr_t& str) {
+//	cs_write("str(len=%u)[", strlen(str));
+//	for (unsigned int i = 0; i < strlen(str); ++i) {
+//		cs_write("%u ", str[i]);
+//	}
+//	cs_write("] ");
+//}
+//
+//template<>
+//void cs_write_val(const char[] str) {
+//	cs_write("str(len=%u)[", strlen(str));
+//	for (unsigned int i = 0; i < strlen(str); ++i) {
+//		cs_write("%u ", str[i]);
+//	}
+//	cs_write("] ");
+//}
+
+//void cs_write_val(const uint8_t* valPtr, size_t valSize) {
+//	cs_write("[");
+//	for (unsigned int i = 0; i < valSize; ++i) {
+//		cs_write("%u ", valPtr[i]);
+//	}
+//	cs_write("] ");
+//}
+//
+//void cs_write_val(const uint8_t& val) {
+//	cs_write_val(&val, sizeof(val));
+//}
+//
+//void cs_write_val(const uint16_t& val) {
+//	cs_write_val(reinterpret_cast<uint8_t*>(&val), sizeof(val));
+//}
+//
+//void cs_write_val(const uint32_t& val) {
+//	cs_write_val(reinterpret_cast<uint8_t*>(&val), sizeof(val));
+//}
+//
+//void cs_write_val(const uint64_t& val) {
+//	cs_write_val(reinterpret_cast<uint8_t*>(&val), sizeof(val));
+//}
+//
+//void cs_write_val(const int8_t& val) {
+//	cs_write_val(reinterpret_cast<uint8_t*>(&val), sizeof(val));
+//}
+//
+//void cs_write_val(const int16_t& val) {
+//	cs_write_val(reinterpret_cast<uint8_t*>(&val), sizeof(val));
+//}
+//
+//void cs_write_val(const int32_t& val) {
+//	cs_write_val(reinterpret_cast<uint8_t*>(&val), sizeof(val));
+//}
+//
+//void cs_write_val(const int64_t& val) {
+//	cs_write_val(reinterpret_cast<uint8_t*>(&val), sizeof(val));
+//}
+//
+//void cs_write_val(const char *str) {
+//	cs_write_val(reinterpret_cast<uint8_t*>(&str), strlen(str));
+//}
+
+template<class... Args>
+void cs_write_args(const Args&... args) {
+	cs_write("args=");
+	(cs_write_val(args), ...);
+	cs_write(SERIAL_CRLF);
+}
+
+
+//#define cs_write_args_x(N, ...) CONCAT_2(cs_write_args_, N) (__VA_ARGS__)
+//#define cs_write_args(...) cs_write_args_x(NUM_VA_ARGS(__VA_ARGS__), __VA_ARGS__)
+//
+//
+//#define cs_write_args_1(arg0) cs_write_val(arg0)
+//#define cs_write_args_2(arg0, arg1) cs_write_val(arg0); cs_write_val(arg1)
+//#define cs_write_args_3(arg0, arg1, arg2) cs_write_val(arg0); cs_write_val(arg1); cs_write_val(arg2)
+//#define cs_write_args_4(arg0, arg1, arg2, arg3) cs_write_val(arg0); cs_write_val(arg1); cs_write_val(arg2); cs_write_val(arg3)
+//#define cs_write_args_5(arg0, arg1, arg2, arg3, arg4) cs_write_val(arg0); cs_write_val(arg1); cs_write_val(arg2); cs_write_val(arg3); cs_write_val(arg4)
+//#define cs_write_args_6(arg0, arg1, arg2, arg3, arg4, arg5) cs_write_val(arg0); cs_write_val(arg1); cs_write_val(arg2); cs_write_val(arg3); cs_write_val(arg4); cs_write_val(arg5)
+//#define cs_write_args_7(arg0, arg1, arg2, arg3, arg4, arg5, arg6) cs_write_val(arg0); cs_write_val(arg1); cs_write_val(arg2); cs_write_val(arg3); cs_write_val(arg4); cs_write_val(arg5); cs_write_val(arg6)
+//#define cs_write_args_8(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7) cs_write_val(arg0); cs_write_val(arg1); cs_write_val(arg2); cs_write_val(arg3); cs_write_val(arg4); cs_write_val(arg5); cs_write_val(arg6); cs_write_val(arg7)
+
+
+
+
+
+//#ifdef __cplusplus
+//}
+//#endif
