54e5fc124e0f4bbc500ea02eebe9d6a23bab7ebf
vliedel <bart@crownstone.rocks>
2021-01-27 12:04:46 +0100

    Initial commit of memory usage tests.

---
+++ source/include/test/cs_MemUsageTest.h
@@ -0,0 +1,54 @@
+/*
+ * Author: Crownstone Team
+ * Copyright: Crownstone (https://crownstone.rocks)
+ * Date: Jan 20, 2021
+ * License: LGPLv3+, Apache License 2.0, and/or MIT (triple-licensed)
+ */
+
+#pragma once
+
+#include <cfg/cs_Boards.h>
+
+class MemUsageTest {
+public:
+	MemUsageTest(const boards_config_t& boardsConfig);
+
+	void start();
+
+	/**
+	 * To be called each tick.
+	 */
+	void onTick();
+
+private:
+	boards_config_t _boardConfig;
+	bool _started = false;
+
+	int _behaviourIndex = 0;
+
+	int _rssiDataStoneId = 1;
+
+	int _presenceProfileId = 0;
+	int _presenceLocationId = 0;
+
+	int _stateType = 0;
+
+	// Returns true when done with all.
+	bool setNextBehaviour();
+
+	// Returns true when done with all.
+	bool sendNextRssiData();
+
+	// Returns true when done with all.
+	bool sendNextPresence();
+
+	// Returns true when done with all.
+	bool setNextStateType();
+
+	// Returns true on success.
+	bool setNextStateType(int stateType);
+
+	void printRamStats();
+};
+
+
---
+++ source/src/test/cs_MemUsageTest.cpp
@@ -0,0 +1,226 @@
+/*
+ * Author: Crownstone Team
+ * Copyright: Crownstone (https://crownstone.rocks)
+ * Date: Jan 20, 2021
+ * License: LGPLv3+, Apache License 2.0, and/or MIT (triple-licensed)
+ */
+
+#include <test/cs_MemUsageTest.h>
+#include <events/cs_Event.h>
+#include <cs_Crownstone.h>
+
+MemUsageTest::MemUsageTest(const boards_config_t& boardsConfig):
+	_boardConfig(boardsConfig) {
+
+}
+
+void MemUsageTest::start() {
+	_started = true;
+}
+
+void MemUsageTest::onTick() {
+	if (!_started) {
+		return;
+	}
+
+	if (setNextBehaviour() == false) {
+		printRamStats();
+		return;
+	}
+
+	if (setNextStateType() == false) {
+		printRamStats();
+		return;
+	}
+
+	if (sendNextPresence() == false) {
+		printRamStats();
+		return;
+	}
+
+	if (sendNextRssiData() == false) {
+		printRamStats();
+		return;
+	}
+}
+
+void MemUsageTest::printRamStats() {
+	Crownstone::updateHeapStats();
+	Crownstone::updateMinStackEnd();
+	Crownstone::printLoadStats();
+}
+
+bool MemUsageTest::setNextBehaviour() {
+	// Can't add all behaviours at once, as that prints too much and crashes the firmware.
+	if (_behaviourIndex >= 50) {
+		return true;
+	}
+	cs_result_t result;
+	ExtendedSwitchBehaviour behaviour(
+			SwitchBehaviour(
+						100,
+						0,
+						0xFF,
+						TimeOfDay(2 * _behaviourIndex),
+						TimeOfDay(2 * _behaviourIndex + 1),
+						PresenceCondition(
+								PresencePredicate(PresencePredicate::Condition::VacuouslyTrue, PresenceStateDescription()),
+								0
+						)
+				),
+				PresenceCondition(
+						PresencePredicate(PresencePredicate::Condition::VacuouslyTrue, PresenceStateDescription()),
+						0
+				)
+	);
+	uint8_t buf[behaviour.serializedSize()];
+	behaviour.serialize(buf, sizeof(buf));
+
+	LOGd("Add behaviour: serializedSize=%u classSize=%u", sizeof(buf), sizeof(behaviour));
+	event_t event(CS_TYPE::CMD_ADD_BEHAVIOUR, buf, sizeof(buf), result);
+	event.dispatch();
+	LOGd("result=%u", event.result.returnCode);
+
+	++_behaviourIndex;
+	return false;
+}
+
+bool MemUsageTest::sendNextRssiData() {
+	if (_rssiDataStoneId >= 256) {
+		return true;
+	}
+	LOGi("sendNextRssiData id=%i", _rssiDataStoneId);
+	int untilId = _rssiDataStoneId + 1;
+	if (untilId > 256) {
+		untilId = 256;
+	}
+
+	TYPIFY(EVT_RECV_MESH_MSG) msg;
+	msg.type = CS_MESH_MODEL_TYPE_UNKNOWN;
+	msg.rssi = -50;
+	msg.hops = 0;
+
+	for (int id = _rssiDataStoneId; id < untilId; ++id) {
+		for (uint8_t channel = 37; channel < 40; ++channel) {
+			msg.channel = channel;
+			msg.srcAddress = id;
+			event_t event(CS_TYPE::EVT_RECV_MESH_MSG, reinterpret_cast<uint8_t*>(&msg), sizeof(msg));
+			event.dispatch();
+		}
+	}
+
+	_rssiDataStoneId = untilId;
+//	++_rssiDataStoneId;
+	return false;
+}
+
+bool MemUsageTest::sendNextPresence() {
+	if (_presenceProfileId >= 8) {
+		return true;
+	}
+	LOGi("sendNextPresence profile=%i location=%i", _presenceProfileId, _presenceLocationId);
+
+	int locationIdUntil = _presenceLocationId + 8;
+	if (locationIdUntil > 64) {
+		locationIdUntil = 64;
+	}
+
+	TYPIFY(EVT_RECEIVED_PROFILE_LOCATION) profileLocation;
+	profileLocation.fromMesh = true;
+	profileLocation.simulated = false;
+	profileLocation.profileId = _presenceProfileId;
+
+	for (int locationId = _presenceLocationId; locationId < locationIdUntil; ++locationId) {
+		profileLocation.locationId = locationId;
+		event_t event(CS_TYPE::EVT_RECEIVED_PROFILE_LOCATION, reinterpret_cast<uint8_t*>(&profileLocation), sizeof(profileLocation));
+		event.dispatch();
+	}
+
+	if (locationIdUntil >= 64) {
+		// Next profile ID
+		++_presenceProfileId;
+		_presenceLocationId = 0;
+	}
+	else {
+		_presenceLocationId = locationIdUntil;
+	}
+	return false;
+}
+
+bool MemUsageTest::setNextStateType() {
+//	if (_stateType >= 0xFFFF) {
+	if (_stateType >= InternalBaseBluetooth) {
+		return true;
+	}
+
+	int stateTypeUntil = _stateType + 10;
+
+	for (int stateType = _stateType; stateType < stateTypeUntil; ++stateType) {
+		setNextStateType(stateType);
+	}
+
+
+	_stateType = stateTypeUntil;
+//	++_stateType;
+	return false;
+}
+
+bool MemUsageTest::setNextStateType(int intType) {
+	CS_TYPE stateType = toCsType(intType);
+
+	if (stateType == CS_TYPE::CONFIG_DO_NOT_USE) {
+		return false;
+	}
+
+	if (DefaultLocation(stateType) == PersistenceMode::NEITHER_RAM_NOR_FLASH) {
+		return false;
+	}
+
+	if (TypeSize(stateType) == 0) {
+		return false;
+	}
+
+	// Some types shouldn't be set
+	switch (stateType) {
+		// Set by setup:
+		case CS_TYPE::CONFIG_SPHERE_ID:
+		case CS_TYPE::CONFIG_CROWNSTONE_ID:
+		case CS_TYPE::CONFIG_KEY_ADMIN:
+		case CS_TYPE::CONFIG_KEY_MEMBER:
+		case CS_TYPE::CONFIG_KEY_BASIC:
+		case CS_TYPE::CONFIG_KEY_SERVICE_DATA:
+		case CS_TYPE::CONFIG_KEY_LOCALIZATION:
+		case CS_TYPE::CONFIG_MESH_DEVICE_KEY:
+		case CS_TYPE::CONFIG_MESH_APP_KEY:
+		case CS_TYPE::CONFIG_MESH_NET_KEY:
+		case CS_TYPE::CONFIG_IBEACON_MAJOR:
+		case CS_TYPE::CONFIG_IBEACON_MINOR:
+		case CS_TYPE::CONFIG_IBEACON_UUID:
+		// Set by system:
+		case CS_TYPE::STATE_OPERATION_MODE:
+		case CS_TYPE::STATE_MESH_IV_INDEX:
+		case CS_TYPE::STATE_MESH_SEQ_NUMBER:
+		// This messes up the uart:
+		case CS_TYPE::STATE_UART_KEY:
+		// These are set by the behaviour test:
+		case CS_TYPE::STATE_BEHAVIOUR_RULE:
+		case CS_TYPE::STATE_TWILIGHT_RULE:
+		case CS_TYPE::STATE_EXTENDED_BEHAVIOUR_RULE:
+		case CS_TYPE::STATE_BEHAVIOUR_MASTER_HASH:
+			return false;
+		default:
+			break;
+	}
+
+	LOGd("Set type %u", stateType);
+	uint8_t* data = new uint8_t[TypeSize(stateType)];
+	cs_state_data_t stateData(stateType, 0, data, TypeSize(stateType));
+	getDefault(stateData, _boardConfig);
+	cs_ret_code_t retCode = State::getInstance().set(stateData);
+	switch (retCode) {
+		case ERR_SUCCESS:
+		case ERR_SUCCESS_NO_CHANGE:
+			return true;
+	}
+	return false;
+}
