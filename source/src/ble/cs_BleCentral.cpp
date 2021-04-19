98ae035e6d61d47aad6aad00fddb820292906265
vliedel <bart@crownstone.rocks>
2021-04-19 09:31:59 +0200

    Cleaned up logs.

--- source/src/ble/cs_BleCentral.cpp
+++ source/src/ble/cs_BleCentral.cpp
@@ -13,11 +13,16 @@
 #include <events/cs_EventDispatcher.h>
 #include <logging/cs_Logger.h>
 
+#define LOGBleCentralInfo LOGi
+#define LOGBleCentralDebug LOGnone
+#define LogLevelBleCentralDebug SERIAL_VERBOSE
+
 const uint16_t WRITE_OVERHEAD = 3;
 const uint16_t LONG_WRITE_OVERHEAD = 5;
 
 void handle_discovery(ble_db_discovery_evt_t* event) {
-		BleCentral::getInstance().onDiscoveryEvent(*event);
+	// No need for null pointer check.
+	BleCentral::getInstance().onDiscoveryEvent(*event);
 }
 
 BleCentral::BleCentral() {
@@ -35,12 +40,12 @@ void BleCentral::init() {
 
 cs_ret_code_t BleCentral::connect(const device_address_t& address, uint16_t timeoutMs) {
 	if (isBusy()) {
-		LOGi("Busy");
+		LOGBleCentralInfo("Busy");
 		return ERR_BUSY;
 	}
 
 	if (Stack::getInstance().isConnected() || isConnected()) {
-		LOGi("Already connected");
+		LOGBleCentralInfo("Already connected");
 		return ERR_WRONG_STATE;
 	}
 
@@ -76,6 +81,9 @@ cs_ret_code_t BleCentral::connect(const device_address_t& address, uint16_t time
 	scanParams.interval = 160;
 	scanParams.window = 80;
 
+//	State::getInstance().get(CS_TYPE::CONFIG_SCAN_INTERVAL, &scanParams.interval, sizeof(scanParams.interval));
+//	State::getInstance().get(CS_TYPE::CONFIG_SCAN_WINDOW, &scanParams.window, sizeof(scanParams.window));
+
 	if (scanParams.timeout < BLE_GAP_SCAN_TIMEOUT_MIN) {
 		scanParams.timeout = BLE_GAP_SCAN_TIMEOUT_MIN;
 	}
@@ -92,7 +100,7 @@ cs_ret_code_t BleCentral::connect(const device_address_t& address, uint16_t time
 		return ERR_UNSPECIFIED;
 	}
 
-	LOGi("Connecting..");
+	LOGBleCentralInfo("Connecting..");
 
 	// We will get either BLE_GAP_EVT_CONNECTED or BLE_GAP_EVT_TIMEOUT.
 	_currentOperation = Operation::CONNECT;
@@ -101,12 +109,12 @@ cs_ret_code_t BleCentral::connect(const device_address_t& address, uint16_t time
 
 cs_ret_code_t BleCentral::disconnect() {
 	if (isBusy()) {
-		LOGi("Busy");
+		LOGBleCentralInfo("Busy");
 		return ERR_BUSY;
 	}
 
 	if (!isConnected()) {
-		LOGd("Already disconnected");
+		LOGBleCentralInfo("Already disconnected");
 		return ERR_SUCCESS;
 	}
 
@@ -115,7 +123,7 @@ cs_ret_code_t BleCentral::disconnect() {
 	uint32_t nrfCode = sd_ble_gap_disconnect(_connectionHandle, BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
 	switch (nrfCode) {
 		case NRF_SUCCESS: {
-			LOGi("Disconnecting..");
+			LOGBleCentralInfo("Disconnecting..");
 			// We will get BLE_GAP_EVT_DISCONNECTED.
 			_currentOperation = Operation::DISCONNECT;
 			return ERR_WAIT_FOR_SUCCESS;
@@ -146,11 +154,11 @@ bool BleCentral::isBusy() {
 
 cs_ret_code_t BleCentral::discoverServices(const UUID* uuids, uint8_t uuidCount) {
 	if (isBusy()) {
-		LOGi("Busy");
+		LOGBleCentralInfo("Busy");
 		return ERR_BUSY;
 	}
 	if (!isConnected()) {
-		LOGi("Not connected");
+		LOGBleCentralInfo("Not connected");
 		return ERR_WRONG_STATE;
 	}
 
@@ -181,39 +189,9 @@ cs_ret_code_t BleCentral::discoverServices(const UUID* uuids, uint8_t uuidCount)
 }
 
 void BleCentral::onDiscoveryEvent(ble_db_discovery_evt_t& event) {
-	uint32_t retCode;
 	switch (event.evt_type) {
 		case BLE_DB_DISCOVERY_COMPLETE: {
-			LOGi("Discovery found uuid=0x%04X type=%u characteristicCount=%u", event.params.discovered_db.srv_uuid.uuid, event.params.discovered_db.srv_uuid.type, event.params.discovered_db.char_count);
-			if (event.params.discovered_db.srv_uuid.type >= BLE_UUID_TYPE_VENDOR_BEGIN) {
-				ble_uuid128_t fullUuid;
-				uint8_t uuidSize = 0;
-				retCode = sd_ble_uuid_encode(&(event.params.discovered_db.srv_uuid), &uuidSize, fullUuid.uuid128);
-				if (retCode == NRF_SUCCESS && uuidSize == sizeof(fullUuid)) {
-#if CS_SERIAL_NRF_LOG_ENABLED == 0
-					LOGd("Full uuid: %02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X",
-							fullUuid.uuid128[15],
-							fullUuid.uuid128[14],
-							fullUuid.uuid128[13],
-							fullUuid.uuid128[12],
-							fullUuid.uuid128[11],
-							fullUuid.uuid128[10],
-							fullUuid.uuid128[9],
-							fullUuid.uuid128[8],
-							fullUuid.uuid128[7],
-							fullUuid.uuid128[6],
-							fullUuid.uuid128[5],
-							fullUuid.uuid128[4],
-							fullUuid.uuid128[3],
-							fullUuid.uuid128[2],
-							fullUuid.uuid128[1],
-							fullUuid.uuid128[0]);
-#endif
-				}
-				else {
-					LOGw("Failed to get full uuid");
-				}
-			}
+			LOGBleCentralDebug("Discovery found service uuid=0x%04X type=%u characteristicCount=%u", event.params.discovered_db.srv_uuid.uuid, event.params.discovered_db.srv_uuid.type, event.params.discovered_db.char_count);
 
 			// Send an event for the service
 			ble_central_discovery_t packet = {
@@ -236,7 +214,7 @@ void BleCentral::onDiscoveryEvent(ble_db_discovery_evt_t& event) {
 			break;
 		}
 		case BLE_DB_DISCOVERY_SRV_NOT_FOUND: {
-			LOGi("Discovery not found uuid=0x%04X type=%u", event.params.discovered_db.srv_uuid.uuid, event.params.discovered_db.srv_uuid.type);
+			LOGBleCentralDebug("Discovery did not find service uuid=0x%04X type=%u", event.params.discovered_db.srv_uuid.uuid, event.params.discovered_db.srv_uuid.type);
 			break;
 		}
 		case BLE_DB_DISCOVERY_ERROR: {
@@ -249,15 +227,16 @@ void BleCentral::onDiscoveryEvent(ble_db_discovery_evt_t& event) {
 			// Instead, we should be done when the number of registered uuids equals the number of received BLE_DB_DISCOVERY_COMPLETE + BLE_DB_DISCOVERY_SRV_NOT_FOUND events.
 			// See https://devzone.nordicsemi.com/f/nordic-q-a/20846/getting-service-count-from-database-discovery-module
 			// We apply a similar patch to the SDK.
-			LOGi("Discovery done");
+			LOGBleCentralInfo("Discovery done");
 
 			// According to the doc, the "services" struct is only for internal usage.
 			// But it seems to store all the discovered services and characteristics.
+			// Also contains the services that have not been found.
 			for (uint8_t s = 0; s < _discoveryModule.discoveries_count; ++s) {
-				LOGi("service: uuidType=%u uuid=0x%04X", _discoveryModule.services[s].srv_uuid.type, _discoveryModule.services[s].srv_uuid.uuid);
+				LOGBleCentralDebug("service: uuidType=%u uuid=0x%04X", _discoveryModule.services[s].srv_uuid.type, _discoveryModule.services[s].srv_uuid.uuid);
 				for (uint8_t c = 0; c < _discoveryModule.services[s].char_count; ++c) {
-					ble_gatt_db_char_t* characteristic = &(_discoveryModule.services[s].charateristics[c]);
-					LOGi("    char: cccd_handle=0x%X uuidType=%u uuid=0x%04X handle_value=0x%X handle_decl=0x%X",
+					__attribute__((unused)) ble_gatt_db_char_t* characteristic = &(_discoveryModule.services[s].charateristics[c]);
+					LOGBleCentralDebug("    char: cccd_handle=0x%X uuidType=%u uuid=0x%04X handle_value=0x%X handle_decl=0x%X",
 							characteristic->cccd_handle,
 							characteristic->characteristic.uuid.type,
 							characteristic->characteristic.uuid.uuid,
@@ -269,26 +248,21 @@ void BleCentral::onDiscoveryEvent(ble_db_discovery_evt_t& event) {
 			break;
 		}
 	}
-	LOGd("Discovery progress=%u pending=%u numServicesHandled=%u numServices=%u",
-			_discoveryModule.discovery_in_progress,
-			_discoveryModule.discovery_pending,
-			_discoveryModule.discoveries_count,
-			_discoveryModule.srv_count);
 }
 
 cs_ret_code_t BleCentral::write(uint16_t handle, const uint8_t* data, uint16_t len) {
 	if (isBusy()) {
-		LOGi("Busy");
+		LOGBleCentralInfo("Busy");
 		return ERR_BUSY;
 	}
 
 	if (!isConnected()) {
-		LOGi("Not connected");
+		LOGBleCentralInfo("Not connected");
 		return ERR_WRONG_STATE;
 	}
 
 	if (handle == BLE_GATT_HANDLE_INVALID) {
-		LOGi("Invalid handle");
+		LOGBleCentralInfo("Invalid handle");
 		return ERR_WRONG_PARAMETER;
 	}
 
@@ -358,7 +332,7 @@ cs_ret_code_t BleCentral::nextWrite(uint16_t handle, uint16_t offset) {
 		writeParams.len = 0;
 		writeParams.p_value = nullptr;
 	}
-	LOGd("nextWrite offset=%u _bufDataSize=%u writeParams: offset=%u len=%u op=%u", offset, _bufDataSize, writeParams.offset, writeParams.len, writeParams.write_op);
+	LOGBleCentralDebug("nextWrite offset=%u _bufDataSize=%u writeParams: offset=%u len=%u op=%u", offset, _bufDataSize, writeParams.offset, writeParams.len, writeParams.write_op);
 
 	uint32_t nrfCode = sd_ble_gattc_write(_connectionHandle, &writeParams);
 	if (nrfCode != NRF_SUCCESS) {
@@ -376,17 +350,17 @@ cs_ret_code_t BleCentral::nextWrite(uint16_t handle, uint16_t offset) {
 
 cs_ret_code_t BleCentral::read(uint16_t handle) {
 	if (isBusy()) {
-		LOGi("Busy");
+		LOGBleCentralInfo("Busy");
 		return ERR_BUSY;
 	}
 
 	if (!isConnected()) {
-		LOGi("Not connected");
+		LOGBleCentralInfo("Not connected");
 		return ERR_WRONG_STATE;
 	}
 
 	if (handle == BLE_GATT_HANDLE_INVALID) {
-		LOGi("Invalid handle");
+		LOGBleCentralInfo("Invalid handle");
 		return ERR_WRONG_PARAMETER;
 	}
 
@@ -425,13 +399,13 @@ void BleCentral::finalizeOperation(Operation operation, cs_ret_code_t retCode) {
 }
 
 void BleCentral::finalizeOperation(Operation operation, uint8_t* data, uint8_t dataSize) {
-	LOGi("finalizeOperation operation=%u _currentOperation=%u", operation, _currentOperation);
+	LOGBleCentralDebug("finalizeOperation operation=%u _currentOperation=%u", operation, _currentOperation);
 	event_t event(CS_TYPE::CONFIG_DO_NOT_USE);
 	event.data = data;
 	event.size = dataSize;
 	switch (_currentOperation) {
 		case Operation::NONE: {
-			LOGw("No operation was in progress")
+			LOGBleCentralDebug("No operation was in progress")
 			break;
 		}
 		case Operation::CONNECT: {
@@ -533,7 +507,7 @@ void BleCentral::onConnect(uint16_t connectionHandle, const ble_gap_evt_connecte
 	if (event.role != BLE_GAP_ROLE_CENTRAL) {
 		return;
 	}
-	LOGi("Connected");
+	LOGBleCentralInfo("Connected");
 
 	_connectionHandle = connectionHandle;
 
@@ -551,7 +525,7 @@ void BleCentral::onGapTimeout(const ble_gap_evt_timeout_t& event) {
 	if (event.src != BLE_GAP_TIMEOUT_SRC_CONN) {
 		return;
 	}
-	LOGi("Connect timeout");
+	LOGBleCentralInfo("Connect timeout");
 
 	// Connection failed, reset state.
 	_connectionHandle = BLE_CONN_HANDLE_INVALID;
@@ -563,8 +537,7 @@ void BleCentral::onDisconnect(const ble_gap_evt_disconnected_t& event) {
 		// Ignore disconnect event as peripheral.
 		return;
 	}
-
-	LOGi("Disconnected reason=%u", event.reason);
+	LOGBleCentralInfo("Disconnected reason=%u", event.reason);
 
 	// Disconnected, reset state.
 	_connectionHandle = BLE_CONN_HANDLE_INVALID;
@@ -578,13 +551,13 @@ void BleCentral::onMtu(uint16_t gattStatus, const ble_gattc_evt_exchange_mtu_rsp
 		return;
 	}
 	_mtu = event.server_rx_mtu;
-	LOGi("MTU=%u", _mtu);
+	LOGBleCentralInfo("MTU=%u", _mtu);
 	finalizeOperation(Operation::CONNECT, ERR_SUCCESS);
 }
 
 void BleCentral::onRead(uint16_t gattStatus, const ble_gattc_evt_read_rsp_t& event) {
-	_log(SERIAL_INFO, false, "Read offset=%u len=%u data=", event.offset, event.len);
-	_logArray(SERIAL_INFO, true, event.data, event.len);
+	_log(LogLevelBleCentralDebug, false, "Read offset=%u len=%u data=", event.offset, event.len);
+	_logArray(LogLevelBleCentralDebug, true, event.data, event.len);
 
 	// According to https://infocenter.nordicsemi.com/topic/com.nordic.infocenter.s132.api.v6.1.1/group___b_l_e___g_a_t_t_c___v_a_l_u_e___r_e_a_d___m_s_c.html
 	// we should continue reading, until we get a gatt status of invalid offset.
@@ -630,7 +603,7 @@ void BleCentral::onRead(uint16_t gattStatus, const ble_gattc_evt_read_rsp_t& eve
 }
 
 void BleCentral::onWrite(uint16_t gattStatus, const ble_gattc_evt_write_rsp_t& event) {
-	LOGi("onWrite write_op=%u offset=%u len=%u", event.write_op, event.offset, event.len);
+	LOGBleCentralDebug("onWrite write_op=%u offset=%u len=%u", event.write_op, event.offset, event.len);
 
 	if (gattStatus != BLE_GATT_STATUS_SUCCESS) {
 		LOGw("gattStatus=%u", gattStatus);
@@ -680,7 +653,7 @@ void BleCentral::onGattCentralEvent(uint16_t evtId, const ble_gattc_evt_t& event
 		return;
 	}
 	if (event.gatt_status != BLE_GATT_STATUS_SUCCESS) {
-		LOGw("Event id=%u status=%u", evtId, event.gatt_status);
+		LOGBleCentralDebug("Event id=%u status=%u", evtId, event.gatt_status);
 	}
 	switch (evtId) {
 		case BLE_GATTC_EVT_EXCHANGE_MTU_RSP: {
