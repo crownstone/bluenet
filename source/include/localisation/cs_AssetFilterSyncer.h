6744b49994c383bc9c070df002733f0e195ff9f9
vliedel <bart@crownstone.rocks>
2021-05-16 16:09:01 +0200

    Added doc comments to filter syncer class.

--- source/include/localisation/cs_AssetFilterSyncer.h
+++ source/include/localisation/cs_AssetFilterSyncer.h
@@ -15,21 +15,31 @@
  * Class that takes care of synchronizing the asset filters between crownstones.
  *
  * - Regularly informs other crownstones of the master version and CRC.
- * - Will connect and update the asset filters of a crownstone with older master version.
+ * - Will connect and update the asset filters of a crownstone with an older master version.
  */
 class AssetFilterSyncer: EventListener {
 public:
+	/**
+	 * Interval at which the master version is broadcasted.
+	 */
 	constexpr static uint16_t VERSION_BROADCAST_INTERVAL_SECONDS = 5 * 60;
 
+	/**
+	 * Init the class:
+	 * - Starts listening for events.
+	 */
 	cs_ret_code_t init(AssetFilterStore& store);
 
 	/**
-	 * Sends the master version and CRC over the mesh and UART.
+	 * Sends the master version and CRC over the mesh.
 	 */
 	void sendVersion(bool reliable);
 
 
 private:
+	/**
+	 * Async steps that are taken when synchronizing filters to another crownstone.
+	 */
 	enum class SyncStep {
 		NONE,
 		CONNECT,
@@ -40,6 +50,9 @@ private:
 		DISCONNECT
 	};
 
+	/**
+	 * Results of comparing master version with another crownstone.
+	 */
 	enum class VersionCompare {
 		UNKOWN,
 		OLDER,
@@ -47,40 +60,96 @@ private:
 		NEWER
 	};
 
+	/**
+	 * Pointer to the (intialized) filter store.
+	 */
 	AssetFilterStore* _store = nullptr;
 
+	/**
+	 * Current step.
+	 */
 	SyncStep _step = SyncStep::NONE;
+
+	/**
+	 * Next index of filter IDs to upload/remove array.
+	 */
 	uint8_t _nextFilterIndex;
+
+	/**
+	 * Next chunk index to upload.
+	 */
 	uint16_t _nextChunkIndex;
-	uint8_t _filterUploadCount;
+
+	/**
+	 * Filter IDs that should be uploaded.
+	 */
 	uint8_t _filterIdsToUpload[AssetFilterStore::MAX_FILTER_IDS];
-	uint8_t _filterRemoveCount;
+	uint8_t _filterUploadCount;
+
+	/**
+	 * Filter IDs that should be removed.
+	 */
 	uint8_t _filterIdsToRemove[AssetFilterStore::MAX_FILTER_IDS];
+	uint8_t _filterRemoveCount;
 
 
+	/**
+	 * Set the current step of the sync process.
+	 */
 	void setStep(SyncStep step);
+
+	/**
+	 * Abort the sync process.
+	 */
 	void reset();
+
+	/**
+	 * Compare given master version with the master version of this crownstone.
+	 */
 	VersionCompare compareToMyVersion(asset_filter_cmd_protocol_t protocol, uint16_t masterVersion, uint32_t masterCrc);
 
+	/**
+	 * Start updating filters of another crownstone.
+	 */
 	void syncFilters(stone_id_t stoneId);
+
+	/**
+	 * Steps of the sync process.
+	 */
 	void connect(stone_id_t stoneId);
 	void removeNextFilter();
 	void uploadNextFilter();
 	void commit();
 	void disconnect();
+
 	/**
 	 * When the whole sync process to a stone was successful, call done().
 	 */
 	void done();
 
-
+	/**
+	 * Handle a received version mesh message.
+	 */
 	cs_ret_code_t onVersion(stone_id_t stoneId, cs_mesh_model_msg_asset_filter_version_t& packet);
+
+	/**
+	 * Handle a possible change of "filter modification in progress".
+	 */
 	void onModificationInProgress(bool inProgress);
+
+	/**
+	 * Handle steps of the sync process.
+	 */
 	void onConnectResult(cs_ret_code_t retCode);
 	void onDisconnect();
 	void onWriteResult(cs_central_write_result_t& result);
 	void onFilterSummaries(cs_data_t& payload);
+
+	/**
+	 * Handle the tick event.
+	 */
 	void onTick(uint32_t tickCount);
+
 public:
 	/**
 	 * Internal usage.
