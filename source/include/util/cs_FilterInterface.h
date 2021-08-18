ac310f507c33ae14bc49b69cbb3b4500e09271ed
Arend de Jonge <arenddejonge@gmail.com>
2021-08-18 13:43:29 +0200

    documentation for FilterInterface.h

--- source/include/util/cs_FilterInterface.h
+++ source/include/util/cs_FilterInterface.h
@@ -10,12 +10,20 @@
 #include <protocol/cs_AssetFilterPackets.h>
 #include <util/cs_Crc32.h>
 
+
+/**
+ * Used in AssetFiltering as a generic way to query a filter for containment and shortAssetId.
+ */
 class FilterInterface {
 public:
 	virtual ~FilterInterface() = default;
 
 	virtual bool contains(const void* key, size_t keyLengthInBytes) = 0;
 
+	/**
+	 * In most cases a shortAssetId is generated as crc32 from filtered input data.
+	 * This can be overwritten for special use cases like ExactMatchFilter.
+	 */
 	virtual short_asset_id_t shortAssetId(const void* key, size_t keyLengthInBytes) {
 		auto crc = crc32(static_cast<const uint8_t*>(key), keyLengthInBytes, nullptr);
 
@@ -32,6 +40,5 @@ public:
 
 	virtual size_t size() = 0;
 
-	// shortAssetId(key, len) = 0
 	virtual bool isValid() = 0;
 };
