014810386e22ad7090904f7a6af6458e92323aee
Anne van Rossum <anne@crownstone.rocks>
2021-09-29 17:11:29 +0200

    Use separate SDK folder for bluenet vs bluenet-host

--- host/include/nrf_stubs.h
+++ host/include/nrf_stubs.h
@@ -1,16 +1,3 @@
 #pragma once
 
-typedef unsigned char ble_uuid128_t;
-
-typedef struct ble_uuid_t {
-	uint16_t uuid;
-	uint8_t type;
-} ble_uuid_t;
-
-#define BLE_UUID_TYPE_UNKNOWN 0x00
-
-typedef uint32_t ret_code_t;
-
-uint32_t __get_IPSR() {
-	return 0; // Thread mode
-}
+// See https://github.com/mrquincle/nrf5_sdk and then the host branch
