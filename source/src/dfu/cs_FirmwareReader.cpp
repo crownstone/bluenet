d303a21afae428c887ebd25fc356f9a7c910ad4f
Arend de Jonge <arenddejonge@gmail.com>
2021-11-02 17:01:02 +0100

    trying to flesh out mbr adres from linker

--- source/src/dfu/cs_FirmwareReader.cpp
+++ source/src/dfu/cs_FirmwareReader.cpp
@@ -17,25 +17,45 @@
 #define FIRMWAREREADER_LOG_LVL SERIAL_DEBUG
 
 
+
 void firmwareReaderFsEventHandler(nrf_fstorage_evt_t * p_evt)  {
 	LOGd("firmware reader fs event handler called");
 }
 
-// ------- Bluenet FStorage struct -------
-// TODO: change to whole flash mem.
+// --------------------------------------------------------------------------------
+// -------------------- Memory region definitions for fstorage --------------------
+// --------------------------------------------------------------------------------
 
+// TODO: change to whole flash mem for ease of use.
+
+// ------- Bluenet -------
 //NRF_FSTORAGE_DEF(nrf_fstorage_t	firmwareReaderFsInstance) = {
 //	.evt_handler    = firmwareReaderFsEventHandler,
 //	.start_addr     = g_APPLICATION_START_ADDRESS,
 //	.end_addr       = g_APPLICATION_START_ADDRESS + g_APPLICATION_LENGTH,
 //};
 
+
+// ------- Bootloader -------
+//NRF_FSTORAGE_DEF(nrf_fstorage_t	firmwareReaderFsInstance) = {
+//	.evt_handler    = firmwareReaderFsEventHandler,
+//	.start_addr     = 0x71000,
+//	.end_addr       = 0x71000 + 0xD000,
+//};
+
+// ------- MBR -------
+extern int __start_mbr_params_page, __stop_mbr_params_page;
+static const uint32_t start_mbr_params_page = static_cast<uint32_t>(__start_mbr_params_page);
+static const uint32_t stop_mbr_params_page = static_cast<uint32_t>(__stop_mbr_params_page);
+
 NRF_FSTORAGE_DEF(nrf_fstorage_t	firmwareReaderFsInstance) = {
 	.evt_handler    = firmwareReaderFsEventHandler,
-	.start_addr     = 0x71000,
-	.end_addr       = 0x71000 + 0xD000,
+	.start_addr     = start_mbr_params_page,
+	.end_addr       = stop_mbr_params_page,
 };
 
+
+// --------------------------------------------------------------------------------
 
 FirmwareReader::FirmwareReader() :
 	firmwarePrinter([this]() {
@@ -70,10 +90,11 @@ void FirmwareReader::read(FirmwareSection section, uint16_t startIndex, uint16_t
 	// nrfjprog --memrd 0x00026000 --w 8 --n 50
 //	uint32_t nrfCode = nrf_fstorage_read(&firmwareReaderFsInstance, g_APPLICATION_START_ADDRESS, data_out, size);
 
-//	uint32_t applicationStartAddress = g_APPLICATION_START_ADDRESS;
-	uint32_t bootloaderStartAddress = 0x71000;
+	[[maybe_unused]] uint32_t applicationStartAddress = g_APPLICATION_START_ADDRESS;
+	[[maybe_unused]] uint32_t bootloaderStartAddress = 0x71000;
+	[[maybe_unused]] uint32_t mbrAddress = start_mbr_params_page;
 
-	uint32_t readAddress = bootloaderStartAddress;
+	uint32_t readAddress = mbrAddress;
 
 	LOGFirmwareReaderDebug("reading %d bytes from address: 0x%x", size, readAddress + startIndex);
 	uint32_t nrfCode = nrf_fstorage_read(&firmwareReaderFsInstance, readAddress + startIndex, data_out, size);
