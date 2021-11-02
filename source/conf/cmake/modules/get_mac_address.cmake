43d10b39bfa5509f38a5893019929063ef834e60
Anne van Rossum <anne@crownstone.rocks>
2021-11-02 16:15:06 +0100

    Remove dependency on HEXADECIMAL for math in CMake (which means older CMake versions might be used).

--- source/conf/cmake/modules/get_mac_address.cmake
+++ source/conf/cmake/modules/get_mac_address.cmake
@@ -7,7 +7,7 @@ function(get_mac_address SERIAL_NUM MAC_ADDRESS)
 	endif()
 
 	execute_process(
-		COMMAND nrfjprog -f nrf52 --memrd 0x100000A4 --n 8 ${SERIAL_NUM_SWITCH} ${SERIAL_NUM}
+		COMMAND nrfjprog -f nrf52 --memrd 0x100000A4 --w 8 --n 6 ${SERIAL_NUM_SWITCH} ${SERIAL_NUM}
 		RESULT_VARIABLE status
 		OUTPUT_VARIABLE output
 		ERROR_VARIABLE error
@@ -17,21 +17,25 @@ function(get_mac_address SERIAL_NUM MAC_ADDRESS)
 		return()
 	endif()
 
-	string(REGEX MATCH "^0x([0-9a-fA-F]+): *([0-9a-fA-F]+) *([0-9a-fA-F]+)" Tmp ${output})
-	set(Address ${CMAKE_MATCH_1})
-	set(Half1 ${CMAKE_MATCH_2})
-	set(ValueA ${CMAKE_MATCH_3})
+	message(STATUS "Parse mac address output \"${output}\"")
+	string(REGEX MATCH "^0x([0-9a-fA-F]+): *([0-9a-fA-F]+) *([0-9a-fA-F]+) *([0-9a-fA-F]+) *([0-9a-fA-F]+) *([0-9a-fA-F]+) *([0-9a-fA-F]+)" Tmp ${output})
+	set(Byte1 ${CMAKE_MATCH_2})
+	set(Byte2 ${CMAKE_MATCH_3})
+	set(Byte3 ${CMAKE_MATCH_4})
+	set(Byte4 ${CMAKE_MATCH_5})
+	set(Byte5 ${CMAKE_MATCH_6})
+	set(Byte6 ${CMAKE_MATCH_7})
 
-	# The address will be a Random Static Address, denoted by the last bits being 11, hence, the OR with 0xC
-	# at exactly the last two bits (endianness) that belong to the 48 bits of the MAC address.
-	bitwise_or(${ValueA} "0xC000" ValueB)
-	string(TOUPPER "${ValueB}" Half2)
+	# The address will be a Random Static Address
+	# This is denoted by the last bits being 11, hence, the OR with 0xC0
+	# We only do this for the last two bits (w.r.t. endianness in flash) of the total of 48 bits.
+	# These are the first two bits over the air
+	from_hex("0xC0" Mask)
+	bitwise_or(${Byte6} "${Mask}" Byte6StaticAddress)
+	to_hex("${Byte6StaticAddress}" Byte6StaticAddressHex "")
 
-	# Add colons
-	words(${Half1} DelimValue1 0 ":")
-	words(${Half2} DelimValue2 6 ":")
+	set(Address "${Byte6StaticAddressHex}:${Byte5}:${Byte4}:${Byte3}:${Byte2}:${Byte1}")
+	message(STATUS "Address: ${Address}")
 
-	message(STATUS "Address: ${DelimValue2}:${DelimValue1}")
-
-	set(${MAC_ADDRESS} "${DelimValue2}:${DelimValue1}" PARENT_SCOPE)
+	set(${MAC_ADDRESS} "${Address}" PARENT_SCOPE)
 endfunction()
