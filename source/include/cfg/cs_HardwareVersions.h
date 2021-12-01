1cc9401b3a291c51314e78d10734b763356757b0
mrquincle <anne@crownstone.rocks>
2021-12-01 15:33:38 +0100

    Add user-facing strings for Crownstone Built-in Two hardware
    + In cs_HardwareVersions.h

--- source/include/cfg/cs_HardwareVersions.h
+++ source/include/cfg/cs_HardwareVersions.h
@@ -29,11 +29,23 @@ extern "C" {
 	  |  |  |    |  |  |---  Patch number of PCB version
 	  |  |  |    |  |------  Minor number of PCB version
 	  |  |  |    |---------  Major number of PCB version
-	  |  |  |--------------  Product Type: 1 Dev, 2 Plug, 3 Builtin, 4 Guidestone, 5 dongle, 6 Builtin One
+	  |  |  |--------------  Product Type: 1 Dev, 2 Plug, 3 Builtin, 4 Guidestone, 5 dongle, 6 Builtin One, 7 Builtin Two
 	  |  |-----------------  Market: 1 EU, 2 US
 	  |--------------------  Family: 1 Crownstone
 */
 
+/**
+ * The value written to the UICR is done by a target `write_board_version` created by cmake.
+ * This target invokes a tool that extracts the value from the cs_Boards.h file.
+ * The string returned here is a user-facing string that is not used anywhere else.
+ * The reason to use the UICR for this:
+ *   - bootloader and firmware are easier kept in tandem
+ *   - firmware does not need to know itself on which hardware it runs, it can just inspect UICR
+ *   - hardware version is characteristic for the hardware and should not be updateable (UICR)
+ *     - note, hardware patches come of course with a new UICR
+ *   - in test and development it's easy to set/get UICR values (without firmware running)
+ *     - we can check for firmware compatibility in the build system if there are multiple firmwares
+ */
 static inline const char* get_hardware_version() {
 
 	uint32_t hardwareBoard = NRF_UICR->CUSTOMER[UICR_BOARD_INDEX];
@@ -57,6 +69,10 @@ static inline const char* get_hardware_version() {
 	// CROWNSTONE BUILTIN ONES
 	if (hardwareBoard == ACR01B10B) return "10106000000";
 	if (hardwareBoard == ACR01B10D) return "10106000100";
+	
+	// CROWNSTONE BUILTIN TWOS
+	if (hardwareBoard == ACR01B13B) return "10107000100";
+	if (hardwareBoard == ACR01B15A) return "10107000200";
 
 	// CROWNSTONE PLUGS
 	if (hardwareBoard == ACR01B2A) return "10102000100";
