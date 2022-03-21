1542a4512b6bea6fded97042a149bd358ef5a499
Arend de Jonge <arenddejonge@gmail.com>
2022-03-21 18:42:03 +0100

    remove duplicate entries introduced by adding crownstone-application.src.cmake

--- source/conf/cmake/crownstone-application.src.cmake
+++ source/conf/cmake/crownstone-application.src.cmake
@@ -1,6 +1,5 @@
 message(STATUS "crownstone application source files appended to FOLDER_SOURCE")
 message(STATUS ${SOURCE_DIR})
 
-
 list(APPEND FOLDER_SOURCE "${SOURCE_DIR}/util/cs_BitmaskVarSize.cpp")
 list(APPEND FOLDER_SOURCE "${SOURCE_DIR}/util/cs_Hash.cpp")
