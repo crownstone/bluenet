d5c4a91fab9032cebf58ed2d8fd3baaa8edde2b6
Arend de Jonge <arenddejonge@gmail.com>
2022-03-21 18:58:12 +0100

    [no ci] move event classes to application src

--- source/conf/cmake/crownstone-application.src.cmake
+++ source/conf/cmake/crownstone-application.src.cmake
@@ -5,5 +5,12 @@
 message(STATUS "crownstone application source files appended to FOLDER_SOURCE")
 
 
+
+LIST(APPEND FOLDER_SOURCE "${SOURCE_DIR}/events/cs_Event.cpp")
+LIST(APPEND FOLDER_SOURCE "${SOURCE_DIR}/events/cs_EventDispatcher.cpp")
+LIST(APPEND FOLDER_SOURCE "${SOURCE_DIR}/events/cs_EventListener.cpp")
+
 list(APPEND FOLDER_SOURCE "${SOURCE_DIR}/util/cs_BitmaskVarSize.cpp")
 list(APPEND FOLDER_SOURCE "${SOURCE_DIR}/util/cs_Hash.cpp")
+
+
