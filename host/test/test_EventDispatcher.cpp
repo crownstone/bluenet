241670b02e625e031b8b5c262f4329348f8bc9c5
Arend de Jonge <arenddejonge@gmail.com>
2022-03-22 14:39:12 +0100

    implement very basic event listener test

--- host/test/test_EventDispatcher.cpp
+++ host/test/test_EventDispatcher.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
 
 #include <events/cs_Event.h>
+#include <events/cs_EventDispatcher.h>
+#include <events/cs_EventListener.h>
+
 #include <util/cs_Error.h>
+#include <typeinfo>
+
+
+
+class TestListener : public EventListener {
+	private:
+	bool _receivedEvent = false;
+	std::string _name = "";
+	public:
+
+	TestListener(std::string name) : _name(name) {}
+	virtual void handleEvent(event_t & event) override {
+		printf("handle event in %s\n", _name.c_str());
+		_receivedEvent = true;
+	}
+
+	bool receivedEvent () {
+		return _receivedEvent;
+	}
+};
 
 int main() {
-	bool isImplemented = true;
-	assert(isImplemented, "not implemented");
+	TestListener a("a"), b("b");
+
+	EventDispatcher& dispatcher = EventDispatcher::getInstance();
+
+	dispatcher.addListener(&a);
+	dispatcher.addListener(&b);
+	event_t evt(CS_TYPE::EVT_GENERIC_TEST);
+	evt.dispatch();
+
+	assert(a.receivedEvent(), "test listener a didn't receive anything");
+	assert(b.receivedEvent(), "test listener b didn't receive anything");
 
 	return 0;
 }
