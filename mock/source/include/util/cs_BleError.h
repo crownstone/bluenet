fa14f148783acc07f6b641d0cfb1a3228f91b5d8
Arend de Jonge <arenddejonge@gmail.com>
2022-03-22 14:18:13 +0100

    mock cs_Error and cs_BleError

--- mock/source/include/util/cs_BleError.h
+++ mock/source/include/util/cs_BleError.h
@@ -1,3 +1,5 @@
-#include <stdexcept>
+#pragma once
+#include <cstdlib>
 
-#define APP_ERROR_HANDLER(X) throw std::runtime_error("APP_ERROR_HANDLER with argument: X")
+
+#define APP_ERROR_HANDLER(X) exit(X)
