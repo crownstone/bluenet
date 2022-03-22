892a2de181eefdee0a79d73c1989c9c238cd48fe
Arend de Jonge <arenddejonge@gmail.com>
2022-03-22 14:18:38 +0100

    use mocked assert statement in testEventDispatcher

--- host/test/test_EventDispatcher.cpp
+++ host/test/test_EventDispatcher.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
 #include <events/cs_Event.h>
+#include <util/cs_Error.h>
 
 int main() {
+	bool isImplemented = true;
+	assert(isImplemented, "not implemented");
+
 	return 0;
 }
