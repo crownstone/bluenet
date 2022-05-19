0556883363efba2827a955fa455cc0d46dd93eb5
vliedel <bart@crownstone.rocks>
2022-05-19 09:50:57 +0200

    Changed uart_raw_binary_client to uart_combined_client. Updated log client.

--- scripts/log-client.py
+++ scripts/log-client.py
@@ -17,11 +17,14 @@ import logging
 
 try:
     from pkg_resources import packaging
-    bluenet_logs_version_compat = "1.1.3"
-    if packaging.version.parse(BluenetLogs.__version__) < packaging.version.parse(bluenet_logs_version_compat):
+    # When installed from source, the version is prepended with "-git".
+    bluenet_logs_version = BluenetLogs.__version__.split('-')[0]
+    bluenet_logs_version_compat = "1.2.0"
+    if packaging.version.parse(bluenet_logs_version) < packaging.version.parse(bluenet_logs_version_compat):
         print("Update BluenetLogs. There's a newer version available!")
+        print(f"Installed version: {bluenet_logs_version}, required version: {bluenet_logs_version_compat}")
 except ImportError:
-    print("Couldn't check version. Install pkg_resources for that");
+    print("Couldn't check version. Install pkg_resources for that")
 
 defaultLogStringsFile = os.path.abspath(f"{os.path.dirname(os.path.abspath(__file__))}/../build/default/extracted_logs.json")
 
@@ -45,20 +48,25 @@ argParser.add_argument('--verbose',
                        dest="verbose",
                        action='store_true',
                        help='Show verbose output')
-argParser.add_argument('--noise',
-                       '-r',
-                       dest="noise",
+argParser.add_argument('--plaintext',
+                       '-p',
+                       dest="plaintext",
                        action='store_true',
-                       help='Show uart noise output (plaintext data may be eaten by the binary protocol if containing one of: @~\\)')
+                       help='Also print plaintext logs')
 argParser.add_argument('--raw',
                        '-r',
                        dest="raw",
                        action='store_true',
-                       help='Show raw output (may result in interleaved print statements) (overrides the --noise option when both are set)')
+                       help='Show raw output (may result in interleaved print statements)')
+argParser.add_argument('--hex',
+                       '-H',
+                       dest="hex",
+                       action='store_true',
+                       help='Show raw output as hex values')
 args = argParser.parse_args()
 
 if args.verbose:
-	logging.basicConfig(format='%(asctime)s %(levelname)-7s: %(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s %(levelname)-7s: %(message)s', level=logging.DEBUG)
 
 logStringsFileName = args.logStringsFileName
 
@@ -71,47 +79,40 @@ bluenetLogs = BluenetLogs()
 # Set the dir containing the bluenet source code files.
 bluenetLogs.setLogStringsFile(logStringsFileName)
 
+def onRawDataReceived(data):
+    if args.hex:
+        for b in data:
+            print(f"{b:02X} ", end='', flush=True)
+    else:
+        for b in data:
+            print(chr(b), end='', flush=True)
 
-# stub message printer that hooks into the uart event bus when necessary and prints immediately.
-class AuxMessagePrinter():
-    def __init__(self):
-        if args.raw:
-            try:
-                UartEventBus.subscribe(SystemTopics.uartRawData, self.onRawDataReceived)
-                print("printing raw data enabled")
-            except AttributeError as e:
-                print("Failed enabling raw data printer. Are your crownstone python libs up to date?")
-        
-        elif args.noise:
-            try:
-                UartEventBus.subscribe(SystemTopics.uartNoiseData, self.onNoiseDataReceived)
-                print("printing raw data enabled")
-            except AttributeError as e:
-                print("Failed enabling noise data printer. Are your crownstone python libs up to date?")
+if args.raw:
+    try:
+        UartEventBus.subscribe(SystemTopics.uartRawData, onRawDataReceived)
+    except AttributeError as e:
+        print("Failed enabling raw data printer. Are your crownstone python libs up to date?")
 
-    def onRawDataReceived(self, dat):
-        print(dat.decode('utf-8'), end='', flush=True)
-
-    def onNoiseDataReceived(self, dat):
-        print(dat.decode('utf-8'), end='', flush=True)
-
-
-# make an instance 
-auxprinter = AuxMessagePrinter()
+if args.plaintext:
+    try:
+        bluenetLogs.printPlaintextLogs(True)
+    except AttributeError as e:
+        print("Failed enabling plaintext logging. Are your crownstone python libs up to date?")
 
 
 # Init the Crownstone UART lib.
 uart = CrownstoneUart()
 uart.initialize_usb_sync(port=args.device)
 
-# The try except part is just to catch a control+c to gracefully stop the UART lib.
+# The try except part is just to catch a control+c to gracefully stop the libs.
 try:
-	# Simply keep the program running.
-	while True:
-		time.sleep(1)
+    # Simply keep the program running.
+    while True:
+        time.sleep(0.1)
+
 except KeyboardInterrupt:
-	pass
+    pass
 finally:
-	print("\nStopping UART..")
-	uart.stop()
-	print("Stopped")
+    print("\nStopping UART..")
+    uart.stop()
+    print("Stopped")
