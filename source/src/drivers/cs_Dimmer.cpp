6d50ebb0b357cd68f717fd06b9561ded18ceae4e
Arend de Jonge <arenddejonge@gmail.com>
2022-06-09 18:37:16 +0200

    fix naming convention and add double init check to Dimmer

--- source/src/drivers/cs_Dimmer.cpp
+++ source/src/drivers/cs_Dimmer.cpp
@@ -15,25 +15,30 @@
 #include <test/cs_Test.h>
 
 void Dimmer::init(const boards_config_t& board) {
-	hardwareBoard = board.hardwareBoard;
-	pinEnableDimmer = board.pinEnableDimmer;
+	if(_initialized) {
+		LOGw("Dimmer init was already executed.");
+		return;
+	}
+
+	_hardwareBoard = board.hardwareBoard;
+	_pinEnableDimmer = board.pinEnableDimmer;
 	_hasDimmer = board.pinDimmer != PIN_NONE;
 
 	if (!_hasDimmer) {
 		return;
 	}
 
-	if (pinEnableDimmer != PIN_NONE) {
-		nrf_gpio_cfg_output(pinEnableDimmer);
-		nrf_gpio_pin_clear(pinEnableDimmer);
+	if (_pinEnableDimmer != PIN_NONE) {
+		nrf_gpio_cfg_output(_pinEnableDimmer);
+		nrf_gpio_pin_clear(_pinEnableDimmer);
 	}
 
 	TYPIFY(CONFIG_PWM_PERIOD) pwmPeriodUs;
 	State::getInstance().get(CS_TYPE::CONFIG_PWM_PERIOD, &pwmPeriodUs, sizeof(pwmPeriodUs));
 
-	State::getInstance().get(CS_TYPE::STATE_SOFT_ON_SPEED, &softOnfSpeed, sizeof(softOnfSpeed));
+	State::getInstance().get(CS_TYPE::STATE_SOFT_ON_SPEED, &_softOnSpeed, sizeof(_softOnSpeed));
 
-	LOGd("init enablePin=%u dimmerPin=%u inverted=%u period=%u µs softOnSpeed=%u", board.pinEnableDimmer, board.pinDimmer, board.flags.dimmerInverted, pwmPeriodUs, softOnfSpeed);
+	LOGd("init enablePin=%u dimmerPin=%u inverted=%u period=%u µs softOnSpeed=%u", board.pinEnableDimmer, board.pinDimmer, board.flags.dimmerInverted, pwmPeriodUs, _softOnSpeed);
 
 	pwm_config_t pwmConfig;
 	pwmConfig.channelCount = 1;
@@ -43,7 +48,7 @@ void Dimmer::init(const boards_config_t& board) {
 
 	PWM::getInstance().init(pwmConfig);
 
-	initialized = true;
+	_initialized = true;
 }
 
 bool Dimmer::hasDimmer() {
@@ -55,18 +60,18 @@ void Dimmer::start() {
 		return;
 	}
 	LOGd("start");
-	assert(initialized == true, "Not initialized");
-	if (started) {
+	assert(_initialized == true, "Not initialized");
+	if (_started) {
 		return;
 	}
-	started = true;
+	_started = true;
 
 	enable();
 
 	TYPIFY(CONFIG_START_DIMMER_ON_ZERO_CROSSING) startDimmerOnZeroCrossing;
 	State::getInstance().get(CS_TYPE::CONFIG_START_DIMMER_ON_ZERO_CROSSING, &startDimmerOnZeroCrossing, sizeof(startDimmerOnZeroCrossing));
 
-	switch (hardwareBoard) {
+	switch (_hardwareBoard) {
 		case PCA10036:
 		case PCA10040:
 		case PCA10100:
@@ -85,13 +90,13 @@ bool Dimmer::set(uint8_t intensity, bool fade) {
 		return false;
 	}
 	LOGd("set %u fade=%u", intensity, fade);
-	assert(initialized == true, "Not initialized");
-	if (!enabled && intensity > 0) {
+	assert(_initialized == true, "Not initialized");
+	if (!_enabled && intensity > 0) {
 		LOGd("Dimmer not enabled");
 		return false;
 	}
 
-	uint8_t speed = fade ? softOnfSpeed : 100;
+	uint8_t speed = fade ? _softOnSpeed : 100;
 
 	TEST_PUSH_EXPR_D(this, "intensity", intensity);
 	PWM::getInstance().setValue(0, intensity, speed);
@@ -104,16 +109,16 @@ void Dimmer::setSoftOnSpeed(uint8_t speed) {
 		return;
 	}
 	LOGd("setSoftOnSpeed %u", speed);
-	softOnfSpeed = speed;
+	_softOnSpeed = speed;
 }
 
 void Dimmer::enable() {
 	if (!_hasDimmer) {
 		return;
 	}
-	LOGd("enable");
-	if (pinEnableDimmer != PIN_NONE) {
-		nrf_gpio_pin_set(pinEnableDimmer);
+	if (_pinEnableDimmer != PIN_NONE) {
+		LOGd("enable");
+		nrf_gpio_pin_set(_pinEnableDimmer);
+		_enabled = true;
 	}
-	enabled = true;
 }
