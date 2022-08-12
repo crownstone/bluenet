4964f1851088d959e1c7abc7a481488648d7d5d2
Arend de Jonge <arenddejonge@gmail.com>
2022-08-12 17:13:45 +0200

    add stub for release override on behaviour update

---
+++ host/test/scenarios/test_ReleaseOverrideOnBehaviourUpdate.cpp
@@ -0,0 +1,13 @@
+/*
+ * Author: Crownstone Team
+ * Copyright: Crownstone (https://crownstone.rocks)
+ * Date: Aug 12, 2022
+ * License: LGPLv3+, Apache License 2.0, and/or MIT (triple-licensed)
+ */
+
+
+
+
+int main() {
+	return -1;
+}
