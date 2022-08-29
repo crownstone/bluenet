a0ae29b5c96159980ace3a3aa10745839ffd55bb
Arend de Jonge <arenddejonge@gmail.com>
2022-08-29 22:41:54 +0200

    add mock source for nrf_sdm and ble_gap

--- source/conf/cmake/nrf5sdk-platform-host.src.cmake
+++ source/conf/cmake/nrf5sdk-platform-host.src.cmake
@@ -5,5 +5,6 @@ if(NRF5SDK_PLATFORM_HOST_SOURCE_REL)
 	status (WARNING "NRF5SDK_PLATFORM_HOST_SOURCE_REL is non-empty.")
 endif()
 
-#list(APPEND NRF5SDK_PLATFORM_HOST_SOURCE_REL "components/libraries/timer/app_timer_host.cpp")
-list(APPEND NRF5SDK_PLATFORM_HOST_SOURCE_REL "components/softdevice/s132/mock/ble_gap.c")
+list(APPEND NRF5SDK_PLATFORM_HOST_SOURCE_REL "components/libraries/timer/app_timer_host.cpp")
+list(APPEND NRF5SDK_PLATFORM_HOST_SOURCE_REL "components/softdevice/s132/mock/ble_gap.cpp")
+list(APPEND NRF5SDK_PLATFORM_HOST_SOURCE_REL "components/softdevice/s132/mock/nrf_sdm.cpp")
