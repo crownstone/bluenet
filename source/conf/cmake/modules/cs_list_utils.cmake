d3ef3847f17471c87833e3c163e5419b37c0e8ac
Arend de Jonge <arenddejonge@gmail.com>
2022-09-23 18:26:10 +0200

    Add cmake logging utils: LOG[vdiwe] and LogList variation that respect VERBOSITY

--- source/conf/cmake/modules/cs_list_utils.cmake
+++ source/conf/cmake/modules/cs_list_utils.cmake
@@ -10,9 +10,73 @@
 function(print_list preamble listvarname)
 	if(${listvarname})
 		foreach(F IN LISTS ${listvarname})
-			message(STATUS "${preamble} ${F}")
+			message(STATUS "${preamble}${F}")
 		endforeach()
 	else() 
 		message(STATUS "${preamble} << empty >>")
+	endif()
+endfunction()
+
+
+##########################################################
+# util function to print a status message,
+# outputs only if verbosity level is high enoug.
+#
+# @arguments: all arguments are passed on to message(...)
+##########################################################
+
+function(LOGv)
+	if(VERBOSITY GREATER_EQUAL 4)
+		string(ASCII 27 ESCAPE)
+		message(STATUS "${ESCAPE}[90m[V]${ESCAPE}[m " ${ARGV})
+	endif()
+endfunction()
+
+function(LOGd)
+	if(VERBOSITY GREATER_EQUAL 3)
+		string(ASCII 27 ESCAPE)
+		message(STATUS "${ESCAPE}[39m[D]${ESCAPE}[m " ${ARGV})
+	endif()
+endfunction()
+
+function(LOGi)
+	if(VERBOSITY GREATER_EQUAL 2)
+		string(ASCII 27 ESCAPE)
+		message(STATUS "${ESCAPE}[34m[I]${ESCAPE}[m " ${ARGV})
+	endif()
+endfunction()
+
+function(LOGw)
+	if(VERBOSITY GREATER_EQUAL 1)
+		string(ASCII 27 ESCAPE)
+		message(AUTHOR_WARNING "${ESCAPE}[33m[W]${ESCAPE}[m " ${ARGV})
+	endif()
+endfunction()
+
+function(LOGe)
+	string(ASCII 27 ESCAPE)
+	message(FATAL_ERROR "${ESCAPE}[31m[E]${ESCAPE}[m " ${ARGV})
+endfunction()
+
+
+##########################################################
+# util function to print a status messages, for each 
+# element in a list.
+# outputs only if verbosity level is high enough.
+#
+# @arguments: all arguments are passed on to message(...)
+##########################################################
+
+function(LogListV preamble listvarname)
+	if(VERBOSITY GREATER_EQUAL 4)
+		string(ASCII 27 ESCAPE)
+		print_list("${ESCAPE}[90m[V]${ESCAPE}[m ${preamble}" ${listvarname})
+	endif()
+endfunction()
+
+function(LogListD preamble listvarname)
+	if(VERBOSITY GREATER_EQUAL 3)
+		string(ASCII 27 ESCAPE)
+		print_list("${ESCAPE}[39m[D]${ESCAPE}[m ${preamble}" ${listvarname})
 	endif()
 endfunction()
