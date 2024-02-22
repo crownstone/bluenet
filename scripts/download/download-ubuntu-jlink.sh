b4cddabac1d5aa99aa6d137ff64ab1ac84a3662a
BillyNate <billynate@billynate.com>
2024-02-22 14:54:33 +0000

    Updated the JLink download script to JLink version v7.94k and added support for different architectures

--- scripts/download/download-ubuntu-jlink.sh
+++ scripts/download/download-ubuntu-jlink.sh
@@ -4,10 +4,16 @@ usage="$0 <workspace path>"
 
 WORKSPACE_DIR=${1:? "Usage: $usage"}
 
+ARCH=$(dpkg --print-architecture)
+if [ "$ARCH" == "armhf" ]; then
+	ARCH="arm"
+fi
+
 SEGGER_DOWNLOAD_URL=https://www.segger.com/downloads/jlink
 
-DEB_FILE=JLink_Linux_V760b_x86_64.deb
-MD5=4b0e24a040a74117a041807536372846
+DEB_FILE="JLink_Linux_V794k_${ARCH}.deb"
+declare -A MD5
+MD5=( ["arm"]="4c2ccd326cae510b0798c525e705577b" ["arm64"]="1f895a0ea4aeba658d1247ce335b4d9e" ["i386"]="c39f3e1f581da2e712709163d5fa6b76" ["x86_64"]="e001bf6e53f1bab1c44438dc5fefb38a" )
 
 DOWNLOAD_URL="$SEGGER_DOWNLOAD_URL/$DEB_FILE"
 
@@ -18,7 +24,7 @@ cd "$DOWNLOAD_DIR"
 SKIP_DOWNLOAD=0
 if [ -e "$DEB_FILE" ]; then
 	md5=($(md5sum "$DEB_FILE"))
-	if [ "$md5" = "$MD5" ]; then
+	if [ "$md5" = "${MD5[$ARCH]}" ]; then
 		SKIP_DOWNLOAD=1
 	fi
 fi
@@ -28,8 +34,8 @@ if [ $SKIP_DOWNLOAD -eq 1 ]; then
 else
 	wget --no-verbose --show-progress --progress=dot:giga --timestamping --post-data "accept_license_agreement=accepted&non_emb_ctr=confirmed" "$DOWNLOAD_URL"
 	md5=($(md5sum "$DEB_FILE"))
-	if [ "$md5" != "$MD5" ]; then
-		echo "Checksum incorrect: $md5 != $MD5"
+	if [ "$md5" != "${MD5[$ARCH]}" ]; then
+		echo "Checksum incorrect: $md5 != ${MD5[$ARCH]}"
 		exit 1
 	fi
 fi
